aedb458661d873879d1460ffccd9e8eed1c2e8d9
Dan Qian <daniel.j.qian@gmail.com>
2020-05-17 23:44:49 -0400

    refresh tokens

--- api/app.py
+++ api/app.py
@@ -9,10 +9,15 @@ app.config['SECRET_KEY'] = 'super-secret'
 app.debug = True
 
 app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
+app.config['JWT_TOKEN_LOCATION'] =  ['cookies', 'headers']
+app.config['JWT_REFRESH_COOKIE_PATH'] = '/auth/session'
+app.config['JWT_COOKIE_CSRF_PROTECT '] = False
+app.config['JWT_CSRF_IN_COOKIES'] = False
 
 mongo = PyMongo(app)
 
 from api import api
+from auth import auth
 
 app.register_blueprint(api, url_prefix='/api')
-
+app.register_blueprint(auth, url_prefix='/auth')
--- api/auth.py
+++ api/auth.py
@@ -1,18 +1,28 @@
 from app import mongo, app
 
-from flask_jwt import JWT
+from flask import Flask, jsonify, request, Blueprint
+from flask_jwt_extended import (
+    JWTManager, jwt_required, create_access_token,
+    jwt_refresh_token_required, create_refresh_token,
+    get_jwt_identity, set_refresh_cookies, unset_refresh_cookies,
+    get_raw_jwt, get_jti
+)
 import bcrypt
+
+jwt = JWTManager(app)
+auth = Blueprint('auth', __name__)
 
 # TODO replace with db
 users = {}
-ids = {}
+
+MAX_TOKENS = 10
 
 class User():
     def __init__(self, id, username, hashed):
         self.id = id
         self.username = username
         self.hashed = hashed
-
+        self.refresh_tokens = []
 
 
 def authenticate(username, password):
@@ -24,8 +34,59 @@ def authenticate(username, password):
 
     return None
 
-def identity(payload):
-    user_id = payload['identity']
-    return users.get(user_id, None)
+@auth.route('/login', methods=['POST'])
+def login():
+    username = request.json.get('username', None)
+    password = request.json.get('password', None)
 
-jwt = JWT(app, authenticate, identity)
+    user = authenticate(username, password)
+
+    if (user is None):
+        return jsonify({"msg": "Bad username or password"}), 401
+
+    ret = {
+        'access_token': create_access_token(identity=user.id),
+    }
+    refresh_token = create_refresh_token(identity=user.id)
+
+    # TODO replace with db
+    # Register refresh token with account
+    user.refresh_tokens.append(get_jti(refresh_token))
+    if (len(user.refresh_tokens) > 10):
+        user.refresh_tokens.pop(0)
+
+    resp = jsonify(ret)
+    # secure = true?, max_age?
+    set_refresh_cookies(resp, refresh_token)
+
+    return resp, 200
+
+
+@auth.route('/session/refresh', methods=['GET'])
+@jwt_refresh_token_required
+def refresh():
+    current_user = get_jwt_identity()
+
+    # TODO DB
+    user = users[current_user]
+    jti =  get_raw_jwt()['jti']
+    if (jti not in user.refresh_tokens):
+        return jsonify({"msg": "Expired login"}), 401
+
+    ret = {
+        'access_token': create_access_token(identity=current_user),
+    }
+
+    return jsonify(ret), 200
+
+@auth.route('/session/logout', methods=['GET'])
+@jwt_refresh_token_required
+def logout():
+    # TODO DB
+    user = users[get_jwt_identity()];
+    jti = get_raw_jwt()['jti']
+    user.refresh_tokens.remove(jti)
+
+    resp = jsonify({'logout': True})
+    unset_refresh_cookies(resp)
+    return resp, 200
