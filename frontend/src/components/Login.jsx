87cd6c6b40e6ed7b41575a186e0a7bc2b29bb8e1
Dan Qian <daniel.j.qian@gmail.com>
2021-01-11 12:27:57 -0500

    Unstyled material-ui modal for resetting password

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,19 +1,26 @@
 import React, { useState } from "react";
+import axios from "axios";
 
-import {login} from "../util/auth";
+import Modal from '@material-ui/core/Modal';
 
 import {
     useHistory
 } from "react-router-dom";
 
 export default function Login(props) {
+    
+    /* State for handling login */
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [attempted, setAttempted] = useState(false);
 
+    /* State for handling reset password modal */
+    const [resetModalOpen, setResetModalOpen] = useState(false);
+    const [resetEmail, setResetEmail] = useState("");
+
     let history = useHistory();
 
-    async function handleSubmit(event) {
+    async function handleLogin(event) {
         event.preventDefault();
         // TODO alert?
         try {
@@ -29,10 +36,48 @@ export default function Login(props) {
         }
     }
 
+    async function handleReset(event) {
+      event.preventDefault();
+      // TODO alert?
+      try {
+        const response = await axios.post('/api/accounts/reset_password/request', {
+          "username": resetEmail,
+          "reset_url": window.location.protocol + '//' + window.location.host + '/reset_password'
+        });
+        alert("An email has been sent!");
+      } catch {
+        alert("Error requesting password reset");
+      }
+
+    }
+
+
+
+    const ResetModal = (        
+      <Modal
+        open={resetModalOpen}
+        onClose={() => setResetModalOpen(false)}
+      >
+        <div>          
+          <form onSubmit={handleReset}>
+            <label>
+              Email:
+              <input
+                  autoFocus
+                  type="text"
+                  value={resetEmail}
+                  onChange={e => setResetEmail(e.target.value)}
+              />
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
+      </Modal>
+    )
 
     return (
       <div className="Hophacks">
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleLogin}>
           <label>
             Email:
             <input
@@ -53,7 +98,11 @@ export default function Login(props) {
           <input type="submit" value="Submit" />
         </form>
         <p>{attempted ? "Incorrect Username or Password" : ""}</p>
+        <button type="button" onClick={() => setResetModalOpen(true)}>
+          Reset Password
+        </button>
 
+        {ResetModal}
 
       </div>
     );
