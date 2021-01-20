79eb5e8cfa9006f8ee573895fc45d28359c47e80
Marc Helou <marcerino15@gmail.com>
2021-01-19 19:35:18 -0500

    Start of front-end work

--- frontend/src/components/admin/Admin.jsx
+++ frontend/src/components/admin/Admin.jsx
@@ -2,7 +2,23 @@ import React from "react";
 import { withAdminAuth } from "../../util/auth";
 
 const Admin =  function() {
-    return (<div> Admin Page </div>);
+    return (
+        <div AdminPage>
+            <h1> Blacklist Users </h1>
+          <form onSubmit={handleSubmit}>
+            <label>
+              Email:
+              <input
+                  autoFocus
+                  type="text"
+                  value={email}
+                  onChange={e => setEmail(e.target.value)}
+              />
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
+      );
 };
 
 export default withAdminAuth(Admin);
