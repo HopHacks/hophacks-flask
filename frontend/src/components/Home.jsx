e4f0f7b2baca4f2e9ef0855990e85e43c17fdc04
Jimmy Shi <jimmyshi360@gmail.com>
2021-01-25 17:49:25 -0500

    added basic image and material about card

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,84 +1,25 @@
 import React from "react";
-import { ParallaxBanner } from 'react-scroll-parallax';
-import { ParallaxProvider } from 'react-scroll-parallax';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
 
 export default function Home() {
     return (
-        <ParallaxProvider>
-            <ParallaxBanner
-                layers={[
-                    {
-                        image: '/images/f20/parallax1.webp',
-                        image: '/images/f20/parallax1.png',
-                        amount: 0.9,
-                    },
-                    {
-                        image: '/images/f20/parallax2.webp',
-                        image: '/images/f20/parallax2.png',
-                        amount: 0.825,
-                    },
-                    {
-                        image: '/images/f20/parallax3.webp',
-                        image: '/images/f20/parallax3.png',
-                        amount: 0.75,
-                    },
-                    {
-                        image: '/images/f20/parallax4.webp',
-                        image: '/images/f20/parallax4.png',
-                        amount: 0.675,
-                    },
-                    {
-                        image: '/images/f20/parallax5.webp',
-                        image: '/images/f20/parallax5.png',
-                        amount: 0.6,
-                    },
-                    {
-                        image: '/images/f20/parallax6.webp',
-                        image: '/images/f20/parallax6.png',
-                        amount: 0.525,
-                    },
-                    {
-                        image: '/images/f20/parallax7.webp',
-                        image: '/images/f20/parallax7.png',
-                        amount: 0.45,
-                    },
-                    {
-                        image: '/images/f20/parallax8.webp',
-                        image: '/images/f20/parallax8.png',
-                        amount: 0.375,
-                    },
-                    {
-                        image: '/images/f20/parallax9.webp',
-                        image: '/images/f20/parallax9.png',
-                        amount: 0.3,
-                    },
-                    {
-                        image: '/images/f20/parallax10.webp',
-                        image: '/images/f20/parallax10.png',
-                        amount: 0.225,
-                    },
-                    {
-                        image: '/images/f20/parallax11.webp',
-                        image: '/images/f20/parallax11.png',
-                        amount: 0.15,
-                    },
-                    {
-                        image: '/images/f20/parallax12.webp',
-                        image: '/images/f20/parallax12.png',
-                        amount: 0.075,
-                    },
-                    {
-                        image: '/images/f20/parallax13.webp',
-                        image: '/images/f20/parallax13.png',
-                        amount: 0,
-                    },
-                ]}
+        <>
+            <img src={process.env.PUBLIC_URL + '/images/parallax1.png'} />
+            <Card>
+                <CardContent>
+                    <Typography color="textSecondary" gutterBottom>
+                        About
+                    </Typography>
+                    <Typography color="textSecondary">
+                        HopHacks is a 36-hour biannual Hackathon held at Johns Hopkins University that encourages engineers, designers, and entrepreneurs to explore new ideas and create new applications. Teams of up to 4 university students work on projects from scratch. At the end of the hackathon, teams present their projects to judges and compete for prizes up to $1024!
 
-                style={{
-                    height: '100%',
-                }}
-            />
-        </ParallaxProvider>
+                        Note: Due to the COVID-19 pandemic, we will be hosting HopHacks virtually via Discord and Zoom.
+                    </Typography>
+                </CardContent>
+            </Card>
+        </>
     );
 
 }
