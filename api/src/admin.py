64a93c000e3dc322ed8dcc5f2e2f405dc9cc1cde
tottiliyt <58280109+tottiliyt@users.noreply.github.com>
2021-04-22 14:48:02 -0400

    add pagination

--- api/src/admin.py
+++ api/src/admin.py
@@ -11,3 +11,32 @@ admin_api = Blueprint('admin', __name__)
 @check_admin
 def test_admin():
     return jsonify({'is_admin': True}), 200
+
+@admin_api.route('/users', methods=['GET'])
+@jwt_required
+@check_admin
+def get_all_users_account():
+    query = request.args.get('query')
+    page_num = int(request.args.get('page_num'))
+    school = request.args.get('school')
+    event = request.args.get('event')
+    status = request.args.get('status')
+
+
+
+    # Calculate number of documents to skip
+    skips = 20 * (page_num - 1)
+
+    cursor  = db.users.find({ "$or": [{"username" : {"$regex" : ".*"+query+".*", "$options" : "i"}}, {"profile.first_name": {"$regex" : ".*"+query+".*", "$options" : "i"}}, {"profile.first_name": {"$regex" : ".*"+query+".*", "$options" : "i"}}]})
+
+    # Skip and limit
+    cursor = cursor.skip(skips).limit(20)
+    
+    users = []
+    
+    for document in cursor:
+        users.append(document['profile'])
+        
+    return {'users': users}, 200
+
+
--- frontend/src/components/admin/Admin.jsx
+++ frontend/src/components/admin/Admin.jsx
@@ -1,8 +1,55 @@
-import React from "react";
+import React, {useState,useEffect} from "react";
 import { withAdminAuthCheck } from "../../util/auth";
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Pagination from '@material-ui/lab/Pagination';
+import SearchBar from "material-ui-search-bar";
+import axios from "axios";
+
 
 const Admin =  function() {
-    return (<div> Admin Page </div>);
+    const [page, setPage] = useState(1);
+    const [users, setusers] = useState([]);
+    const [query, setQuery] = useState("");
+    const [event, setEvent] = useState("");
+    const [school, setSchool] = useState("");
+    const [status, setStatus] = useState("");
+
+    const handleChange = (event, value) => {
+      setPage(value);
+    };
+
+    async function getUsers(){
+      const response = await axios.get('/api/admin/users' + '?page_num=' + page + '&query=' + query);
+      setusers(response.data.users);
+    }
+
+
+    useEffect(() => {
+      getUsers()
+  }, []);
+
+    const populateUsers = 
+        users.map((user, index) => 
+          <div key={index} style={{color:"white"}}>
+            {index} - {user.first_name} {user.last_name}
+          </div>
+        );
+
+    return (
+      <>
+      <SearchBar
+    value={query}
+    onChange={(newValue) => setQuery(newValue)}
+    onRequestSearch={() => getUsers()}
+  />
+      {populateUsers}
+      </>
+
+    );
 };
 
 export default withAdminAuthCheck(Admin);
+
+
+
