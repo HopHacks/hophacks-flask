bcb9555510054d9711ea9fbd10fa61540bce4b3a
Nicholas Bowen <nbowen3@jhu.edu>
2022-03-06 16:03:25 -0500

    added ability to create an event

--- api/src/events.py
+++ api/src/events.py
@@ -1,3 +1,4 @@
+from tabnanny import check
 from util.decorators import check_admin
 from db import db
 
@@ -12,3 +13,29 @@ events_api = Blueprint('events', __name__)
 @events_api.route('/', methods=['GET'])
 def get():
     return jsonify("helllo"), 200
+
+
+@events_api.route('/', methods=['POST'])
+# @jwt_required
+# @check_admin
+def create_event():
+    if (request.json is None):
+        return Response('Data not in json format', status=400)
+
+    if not (all(field in request.json for field in ['event_name', 'display_name', 'start_date', 'end_date'])):
+        return Response('Invalid request', status=400)
+
+    event = {}
+
+    event['event_name'] = request.json['event_name']
+    event['display_name'] = request.json['display_name']
+    event['start_date'] = request.json['start_date']
+    event['end_date'] = request.json['end_date']
+    if 'description' in request.json:
+        event['description'] = request.json['description']
+    else:
+        event['description'] = ""
+
+    db.events.insert_one(event)
+
+    return jsonify({"msg": "event added"}), 200
