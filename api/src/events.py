d546e4e99ee892f126a881aa08725224c39bbd35
AkhilDeo <singingbird99@gmail.com>
2022-03-11 16:22:16 -0500

    Updated GET to address new fields

--- api/src/events.py
+++ api/src/events.py
@@ -24,33 +24,41 @@ def get():
             if e['start_date'] >= request.args['start_date'] and e['end_date'] <= request.args['end_date']:
                 if 'description' in e.args:
                     events.append({'event_name': e['event_name'], 'display_name': e['display_name'],
-                                  'start_date': e['start_date'], 'end_date': e['end_date'], 'description': e['description']})
+                                  'start_date': e['start_date'], 'end_date': e['end_date'], 'description': e['description'],
+                                   'num_registrations': e['num_registrations'], 'event_participants': e['event_participants']})
                 else:
                     events.append({'event_name': e['event_name'], 'display_name': e['display_name'],
-                                  'start_date': e['start_date'], 'end_date': e['end_date']})
+                                  'start_date': e['start_date'], 'end_date': e['end_date'],
+                                  'num_registrations': e['num_registrations'], 'event_participants': e['event_participants']})
         elif 'start_date' in request.args:
-                if e['start_date'] >= request.args['start_date']:
-                    if 'description' in e.args:
-                        events.append({'event_name': e['event_name'], 'display_name': e['display_name'],
-                                      'start_date': e['start_date'], 'end_date': e['end_date'], 'description': e['description']})
-                    else:
-                        events.append({'event_name': e['event_name'], 'display_name': e['display_name'],
-                                      'start_date': e['start_date'], 'end_date': e['end_date']})
+            if e['start_date'] >= request.args['start_date']:
+                if 'description' in e.args:
+                    events.append({'event_name': e['event_name'], 'display_name': e['display_name'],
+                                  'start_date': e['start_date'], 'end_date': e['end_date'], 'description': e['description'],
+                                  'num_registrations': e['num_registrations'], 'event_participants': e['event_participants']})
+                else:
+                    events.append({'event_name': e['event_name'], 'display_name': e['display_name'],
+                                  'start_date': e['start_date'], 'end_date': e['end_date'],
+                                  'num_registrations': e['num_registrations'], 'event_participants': e['event_participants']})
         elif 'end_date' in request.args:
-                if e['end_date'] <= request.args['start_date']:
-                    if 'description' in e.args:
-                        events.append({'event_name': e['event_name'], 'display_name': e['display_name'],
-                                      'start_date': e['start_date'], 'end_date': e['end_date'], 'description': e['description']})
-                    else:
-                        events.append({'event_name': e['event_name'], 'display_name': e['display_name'],
-                                      'start_date': e['start_date'], 'end_date': e['end_date']})
+            if e['end_date'] <= request.args['start_date']:
+                if 'description' in e.args:
+                    events.append({'event_name': e['event_name'], 'display_name': e['display_name'],
+                                  'start_date': e['start_date'], 'end_date': e['end_date'], 'description': e['description'],
+                                  'num_registrations': e['num_registrations'], 'event_participants': e['event_participants']})
+                else:
+                    events.append({'event_name': e['event_name'], 'display_name': e['display_name'],
+                                  'start_date': e['start_date'], 'end_date': e['end_date'],
+                                  'num_registrations': e['num_registrations'], 'event_participants': e['event_participants']})
         else:
             if 'description' in e:
                 events.append({'event_name': e['event_name'], 'display_name': e['display_name'],
-                              'start_date': e['start_date'], 'end_date': e['end_date'], 'description': e['description']})
+                              'start_date': e['start_date'], 'end_date': e['end_date'], 'description': e['description'],
+                              'num_registrations': e['num_registrations'], 'event_participants': e['event_participants']})
             else:
                 events.append({'event_name': e['event_name'], 'display_name': e['display_name'],
-                              'start_date': e['start_date'], 'end_date': e['end_date']})
+                              'start_date': e['start_date'], 'end_date': e['end_date'],
+                              'num_registrations': e['num_registrations'], 'event_participants': e['event_participants']})
 
     return jsonify({'events': events}), 200
 
@@ -82,8 +90,6 @@ def create_event():
     return jsonify({"msg": "event added"}), 200
 
 
-
-
 @events_api.route('/', methods=['DELETE'])
 # @jwt_required
 # @check_admin
@@ -95,11 +101,12 @@ def delete_event():
         return Response('Invalid request', status=400)
     event = {}
     event['event_name'] = request.json['event_name']
-    if  len(list(db.events.find(event))) == 0: 
+    if len(list(db.events.find(event))) == 0:
         return Response('Event Does Not Exist', status=400)
-    
+
     db.events.delete_one(event)
     return jsonify({"msg": "event deleted"}), 200
+
 
 @events_api.route('/', methods=['PUT'])
 # @jwt_required
@@ -107,51 +114,46 @@ def delete_event():
 def update_event():
     if (request.json is None):
         return Response('Data not in json format', status=400)
-    
+
     if not (all(field in request.json for field in ['event_name'])):
         return Response('Invalid request', status=400)
-    
+
     event_current = {}
     event_current['event_name'] = request.json['event_name']
-    
-    if len(list(db.events.find(event_current))) == 0: 
+
+    if len(list(db.events.find(event_current))) == 0:
         return Response('Event Does Not Exist', status=400)
-    
+
     cursor = db.events.find(event_current)
     if 'display_name' in request.json:
-        db.events.update_one(event_current, { "$set": { 
-            'display_name': request.json['display_name'] }
+        db.events.update_one(event_current, {"$set": {
+            'display_name': request.json['display_name']}
         })
     if 'start_date' in request.json:
-        db.events.update_one(event_current, { "$set": {
-            'start_date': datetime.strptime(request.json['start_date'], '%m-%d-%Y') }
-        })    
+        db.events.update_one(event_current, {"$set": {
+            'start_date': datetime.strptime(request.json['start_date'], '%m-%d-%Y')}
+        })
     if 'end_date' in request.json:
-        db.events.update_one(event_current, { "$set": {
-            'end_date': datetime.strptime(request.json['end_date'], '%m-%d-%Y') }              
+        db.events.update_one(event_current, {"$set": {
+            'end_date': datetime.strptime(request.json['end_date'], '%m-%d-%Y')}
         })
     if 'description' in request.json:
-        db.events.update_one(event_current, { "$set": { 
-            'description': request.json['description'] }
+        db.events.update_one(event_current, {"$set": {
+            'description': request.json['description']}
         })
-        
+
     # These two fields may be deleted, as they technically shouldn't need to ever be altered
     if 'num_registrations' in request.json:
-        db.events.update_one(event_current, { "$set": { 
-            'num_registrations': request.json['num_registrations'] }
+        db.events.update_one(event_current, {"$set": {
+            'num_registrations': request.json['num_registrations']}
         })
     if 'event_participants' in request.json:
-        db.events.update_one(event_current, { "$set": { 
-            'event_participants': request.json['event_participants'] }
-        }) 
-        
-           
+        db.events.update_one(event_current, {"$set": {
+            'event_participants': request.json['event_participants']}
+        })
+
     if 'new_event_name' in request.json:
-        db.events.update_one(event_current, { "$set": { 
-            'event_name': request.json['new_event_name'] }
+        db.events.update_one(event_current, {"$set": {
+            'event_name': request.json['new_event_name']}
         })
     return jsonify({"msg": "event updated"}), 200
-
-    
-
-    
