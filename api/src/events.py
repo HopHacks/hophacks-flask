059415d4dfa10dce84d09f87886461fe24c6abd9
Nicholas Bowen <nbowen3@jhu.edu>
2022-03-28 20:27:10 -0400

    added ability to update registration and get registrations for a user

--- api/src/events.py
+++ api/src/events.py
@@ -12,6 +12,8 @@ events_api = Blueprint('events', __name__)
 
 # Event names need to be in the format Title_Season_Year. This method
 # enforces that and makes sure that everything is valid
+
+
 def check_event_name(event_name):
     names = ['AlumniPanel', 'Hackathon', 'InterviewWorkshop']
     seasons = ['Fall', 'Winter', 'Spring', 'Summer']
@@ -20,11 +22,11 @@ def check_event_name(event_name):
 
     params = event_name.split('_')
     if len(params) != 3:
-      return False
+        return False
 
     year = int(params[2])
     if (params[0] not in names) or (params[1] not in seasons) or (year < beg_year or year > end_year):
-      return False
+        return False
 
     return True
 
@@ -82,9 +84,11 @@ def create_event():
 
     # Get event name and make sure its valid
     event['event_name'] = request.json['event_name']
-    if not check_event_name(event['event_name']): return Response('Invalid event name', status=400)
+    if not check_event_name(event['event_name']):
+        return Response('Invalid event name', status=400)
     # TODO: do we want to fold the next line into check_event_name or keep check_event_name just for checking the validity of the string itself
-    if len(list(db.events.find({'event_name': event['event_name']}))) != 0: return Response('Event name already exists', status=400)
+    if len(list(db.events.find({'event_name': event['event_name']}))) != 0:
+        return Response('Event name already exists', status=400)
 
     event['display_name'] = request.json['display_name']
     event['start_date'] = datetime.fromisoformat(request.json['start_date'])
@@ -162,28 +166,31 @@ def add_participant():
     event_name = request.json["event_name"]
     user_id = request.json["user_id"]
 
-    event = {'event_name' : event_name}
+    event = {'event_name': event_name}
     if len(list(db.events.find(event))) == 0:
         return Response('Event Does Not Exist', status=400)
 
     user = db.users.find_one({"_id": ObjectId(user_id)})
     record = {
-      'user_id': user_id,
-      'username': user['username'],
-      'profile': user['profile'],
-      'status': 'applied'
+        'user_id': user_id,
+        'username': user['username'],
+        'profile': user['profile'],
+        'status': 'applied'
     }
 
-    db.events.update_one(event, {'$push' : {'event_participants' : record}, '$inc': {'num_registrations': 1}})
+    db.events.update_one(event, {'$push': {'event_participants': record}, '$inc': {
+                         'num_registrations': 1}})
 
     event_record = {
-      'event_name': event_name,
-      'status': 'applied'
+        'event_name': event_name,
+        'status': 'applied'
     }
-    
-    db.users.update_one({"_id": ObjectId(user_id)}, {'$push' : {'registrations' : event_record}})
+
+    db.users.update_one({"_id": ObjectId(user_id)}, {
+                        '$push': {'registrations': event_record}})
 
     return jsonify({"msg": "participant added"}), 200
+
 
 @events_api.route('/update_status', methods=['PUT'])
 # @jwt_required
@@ -192,45 +199,82 @@ def update_status():
     if not (all(field in request.json for field in ['event_name', 'user_id', 'status'])):
         return Response('Invalid request', status=400)
 
-    event_name = request.json["event_name"]
-    user_id = request.json["user_id"]
-    status = request.json["status"]
-    '''
-    event = {'event_name' : event_name}
-    if len(list(db.events.find(event))) == 0:
-        return Response('Event Does Not Exist', status=400)
+    event_name = request.json['event_name']
+    user_id = request.json['user_id']
+    status = request.json['status']
 
+    if status not in ['applied', 'rsvp', 'canceled']:
+        return Response('Invalid request', status=400)
+
+
+    # Update event record
+    event_query = {'event_name': event_name}
+    event = db.events.find_one(event_query)
+    registrations = event['event_participants']
+    found = False
+    for i in range(len(registrations)):
+        user = registrations[i]
+        if user['user_id'] == user_id:
+            found = True
+            registrations[i]['status'] = status
+
+    if not found:
+        return Response('Registration Does Not Exist', status=400)
+
+    db.events.update_one({
+        'event_name': event_name,
+    },
+        {
+            '$set': {'event_participants': registrations}
+    }
+    )
+
+    # Update user record
     user = db.users.find_one({"_id": ObjectId(user_id)})
-    record = {
-      'user_id': user_id,
-      'username': user['username'],
-      'profile': user['profile'],
-      'status': 'applied'
-    }
+    registrations = user['registrations']
+    found = False
+    for i in range(len(registrations)):
+        event = registrations[i]
+        if event['event_name'] == event_name:
+            found = True
+            registrations[i]['status'] = status
 
-    db.events.update_one(event, {'$push' : {'event_participants' : record}, '$inc': {'num_registrations': 1}})
+    if not found:
+        return Response('Registration Does Not Exist', status=400)
+    db.users.update_one({"_id": ObjectId(user_id)},
+        {
+            '$set': {'registrations': registrations}
+        } 
+    )
 
-    event_record = {
-      'event_name': event_name,
-      'status': 'applied'
-    }
-    
-    db.users.update_one({"_id": ObjectId(user_id)}, {'$push' : {'registrations' : event_record}})
-    '''
     return jsonify({"msg": "registration updated"}), 200
+
+
+@events_api.route('/getRegistrations/', methods=['GET'])
+# @jwt_required
+def get_registrations():
+    if 'user_id' not in request.args:
+        return Response('Invalid request', status=400)
+
+    user_id = request.args['user_id']
+    user = db.users.find_one({"_id": ObjectId(user_id)})
+    registrations = user['registrations']
+
+    return jsonify(registrations)
+
 
 @events_api.route('/getParticipants/<event>', methods=['GET'])
 # @jwt_required
 def get_participant(event):
     if event is None:
         return Response('Invalid request', status=400)
-    
-    args = {'event_name' : event}
+
+    args = {'event_name': event}
     if len(list(db.events.find(args))) == 0:
         return Response('Event Does Not Exist', status=400)
-    
+
     # Do not use hopkins_student in arguments (request.json) unless you only want Hopkins Students
-    # If a user being a Hopkins student is irrelevant, just do not include "hopkins_student" in request.json 
+    # If a user being a Hopkins student is irrelevant, just do not include "hopkins_student" in request.json
     participants = []
     cursor = db.events.find(args)
     if 'hopkins_student' in request.args and 'reg_status' in request.args:
@@ -258,12 +302,14 @@ def get_participant(event):
             break
     return jsonify({'participants': participants}), 200
 
+
 def remove_duplicates_from_list(random_list):
     res = []
     for i in random_list:
         if i not in res:
             res.append(i)
     return res
+
 
 @events_api.route('/getParticipants', methods=['GET'])
 # @jwt_required
@@ -298,12 +344,12 @@ def get_participant_by_date():
 
     if len(list(db.events.find(args))) == 0:
         return Response('No events occurred in this time frame', status=400)
-    
+
     participants = []
-    
+
     # Do not use hopkins_student in arguments (request.args) unless you only want Hopkins Students
     # If a user being a Hopkins student is irrelevant, just do not include "hopkins_student" in request.args
-    
+
     if 'hopkins_student' in request.args:
         for i in cursor:
             for j in i['event_participants']:
