df9cf54e672b4e21cf3231d31eb5945966d5b4ba
AkhilDeo <singingbird99@gmail.com>
2022-04-08 16:50:58 -0400

    Channel_info, send_message, and send_message_in_channel ready to be tested

--- api/src/slack.py
+++ api/src/slack.py
@@ -4,3 +4,22 @@ slack_client = SlackClient()
 SLACK_TOKEN = os.environ.get('SLACK_TOKEN', None)
 slack_client = SlackClient(SLACK_TOKEN)
 
+def channel_info(channel_id):
+    channel_info = slack_client.api_call("channels.info", channel=channel_id)
+    if channel_info:
+        return channel_info['channel']
+    return None
+
+def send_message(channel_id, message):
+    slack_client.api_call(
+        "chat.postMessage",
+        channel=channel_id,
+        text=message,
+        username='pythonbot',
+        icon_emoji=':robot_face:'
+    )
+
+@slack_api.route('/', methods = ['POST'])
+def send_message_in_channel(message):
+    send_message(os.environ.get(CHANNEL_TOKEN_TEST), message)
+
