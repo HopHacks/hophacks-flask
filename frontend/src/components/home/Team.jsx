4ccea69c52c4d187bf75037aeaf025caad07d063
Nicholas Bowen <nbowen3@jhu.edu>
2022-06-12 16:17:46 -0400

    added alumni section

--- frontend/src/components/home/Team.jsx
+++ frontend/src/components/home/Team.jsx
@@ -178,6 +178,7 @@ export default function Team() {
               <Tab label="SOCIAL/PR" />
               <Tab label="SPONSORS" />
               <Tab label="WEBSITE" />
+              <Tab label="ALUMNI" />
             </Tabs>
             {value === 0 && <Grid className={classes.team} container>
               <div className={classes.teambox}>
@@ -228,124 +229,15 @@ export default function Team() {
                 <MemberItem imgURL="ShaopengZeng" memberName="Shaopeng Zeng" memberTitle="Website" linkedin="https://www.linkedin.com/in/shaopeng-zeng-862154149/"/>
               </div>
             </Grid>}
+            {value === 5 && <Grid className={classes.team} container>
+              <div className={classes.teambox}>
+                <MemberItem imgURL="brandon_headshot" memberName="Brandon Wong" memberTitle="Nuro" linkedin="https://linkedin.com/in/brandonynwong" github="https://github.com/bwong19"/>
+              </div>
+            </Grid>}
           </Paper>
 
         </CardContent>
       </Card>
     </Box>
-
-    // <Box py={2}>
-    //     <Card>
-    //         <CardContent>
-    //             {/*TODO material UI*/}
-    //             <Typography className={classes.title} variant="h4" gutterBottom>Meet The Team!</Typography>
-
-    //                 <Typography>
-    //                     We're a group of undergraduate students passionate about the intersection of
-    //                     <b> technology</b>,
-    //                     <b> innovation</b>,
-    //                     <b> social good</b>, and
-    //                     <b> fun</b>!
-    //                     <font size="2"> (and pineapple on pizza) </font>
-
-    //                     <p></p>
-    //                 </Typography>
-
-
-    //             <br />
-
-    //             <Box mx="10%">
-
-    //                 {/*TODO make this grid better (spaced with 8? narrower?) */}
-    //                 <Grid className={classes.team} container>
-    //                     <div className = {classes.teambox}>
-    //                     <MemberItem imgURL="marc_headshot" memberName="Marc Helou" memberTitle="Director" />
-    //                     </div>
-
-    //                     <div className = {classes.teambox}>
-    //                     <MemberItem imgURL="brandon_headshot" memberName="Brandon Wong" memberTitle="Head of Membership/ Logistics" />
-    //                     </div>
-
-    //                     <div className = {classes.teambox}>
-    //                     <MemberItem imgURL="alan_headshot" memberName="Alan Li" memberTitle="Head of Website" />
-    //                     </div>
-
-    //                     <div className = {classes.teambox}>
-    //                     <MemberItem imgURL="patrick_headshot" memberName="Patrick Herbert" memberTitle="Head of Logistics" />
-    //                     </div>
-
-    //                     <div className = {classes.teambox}>
-    //                     <MemberItem imgURL="theanh_headshot" memberName="Trần Thế Anh" memberTitle="Head of Logistics/ Sponsors" />
-    //                     </div>
-
-    //                     <div className = {classes.teambox}>
-    //                     <MemberItem imgURL="elizabeth_headshot" memberName="Elizabeth Cho" memberTitle="Head of Design" />
-    //                     </div>
-
-    //                     <div className = {classes.teambox}>
-    //                     <MemberItem imgURL="amber" memberName="Amber Zhou" memberTitle="Co-Head of Judges" />
-    //                     </div>
-
-    //                     <div className = {classes.teambox}>
-    //                     <MemberItem imgURL="stella_headshot" memberName="Stella Li" memberTitle="Co-Head of Judges/ Sponsors" />
-    //                     </div>
-
-    //                     <div className = {classes.teambox}>
-    //                     <MemberItem imgURL="alison_headshot" memberName="Alison Lee" memberTitle="Head of Sponsors/ Design" />
-    //                     </div>
-
-    //                     <div className = {classes.teambox}>
-    //                     <MemberItem imgURL="Neha" memberName="Neha Nandiwada" memberTitle="Logistics" />
-    //                     </div>
-
-    //                     <div className = {classes.teambox}>
-    //                     <MemberItem imgURL="curtis_headshot" memberName="Curtis Ahn" memberTitle="Website" />
-    //                     </div>
-
-    //                     <div className = {classes.teambox}>
-    //                     <MemberItem imgURL="jimmy_headshot" memberName="Jimmy Shi" memberTitle="Design/Website" personal="https://www.jimmyshi.com/" linkedin="https://www.linkedin.com/in/jimmyshi360/" github="https://github.com/jimmyshi360" />
-    //                     </div>
-
-    //                     <div className = {classes.teambox}>
-    //                     <MemberItem imgURL="elaine_headshot" memberName="Elaine He" memberTitle="Design/Website" />
-    //                     </div>
-
-    //                     <div className = {classes.teambox}>
-    //                     <MemberItem imgURL="charissa_headshot" memberName="Charissa Zou" memberTitle="Design/Website" />
-    //                     </div>
-
-    //                     <div className = {classes.teambox}>
-    //                     <MemberItem imgURL="kavan_headshot" memberName="Kavan Bansal" memberTitle="Social/PR/ Sponsors" />
-    //                     </div>
-
-    //                     <div className = {classes.teambox}>
-    //                     <MemberItem imgURL="zoe_headshot" memberName="Zoe Kim" memberTitle="Social & PR/Sponsors" />
-    //                     </div>
-
-    //                     <div className = {classes.teambox}>
-    //                     <MemberItem imgURL="laine_headshot" memberName="Laine Wang" memberTitle="Social/PR" />
-    //                     </div>
-
-    //                     <div className = {classes.teambox}>
-    //                     <MemberItem imgURL="joanne" memberName="Joanne Selinski" memberTitle="Faculty Advisor" />
-    //                     </div>
-
-    //                     <div className = {classes.teambox}>
-    //                     <MemberItem imgURL="kelly" memberName="Kelly Culotta" memberTitle="Admin Coordinator" />
-    //                     </div>
-
-    //                     {/* <MemberItem imgURL="xiangyu" memberName="Xiangyu Shen" memberTitle="Website" /> */}
-
-    //                     {/* <MemberItem imgURL="arielle" memberName="Arielle Summitt" memberTitle="Social/PR" /> */}
-
-    //                 </Grid>
-    //             </Box>
-    //             <br />
-    //             <br />
-
-
-    //         </CardContent>
-    //     </Card>
-    // </Box>
   );
 }
