57301ecf4865556e1039623e278ceb2d93595e7e
Julia <julia.bian92@gmail.com>
2022-06-22 11:35:30 -0700

    deleted files

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -62,7 +62,6 @@ export default function Home() {
         const script2 = document.createElement("script");
         const script3 = document.createElement("script");
         const script4 = document.createElement("script");
-        const script5 = document.createElement("script");
 
         script1.src = "pixi.min.js";
         script1.async = true;
@@ -73,24 +72,19 @@ export default function Home() {
         script3.src = "main.js";
         script3.async = true;
 
-        // script4.src = "imagesloaded.pkgd.min.js";
-        // script4.async = true;
-
-        script5.src = "home.js";
-        script5.async = true;
+        script4.src = "home.js";
+        script4.async = true;
 
         document.body.appendChild(script1);
         document.body.appendChild(script2);
         document.body.appendChild(script3);
-        // document.body.appendChild(script4);
-        document.body.appendChild(script5);
+        document.body.appendChild(script4);
 
         return () => {
             document.body.removeChild(script1);
             document.body.removeChild(script2);
             document.body.removeChild(script3);
-            // document.body.removeChild(script4);
-            document.body.removeChild(script5);
+            document.body.removeChild(script4);
           }
     }, [])
 
