d17e406722645601aad146720f190c5aad5584fb
MuxiLyuLucy <muxilyu2004@gmail.com>
2022-06-21 23:54:36 -0400

    a very rough draft of carousels

--- frontend/src/components/home/Sponsors.jsx
+++ frontend/src/components/home/Sponsors.jsx
@@ -7,6 +7,15 @@ import CardContent from '@material-ui/core/CardContent';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
+
+import ReactDOM from "react-dom";
+import { Controller, Scene } from "react-scrollmagic";
+import { ParallaxProvider, Parallax } from "react-scroll-parallax";
+import { useWindowSize } from "react-use";
+import { useTrail, animated } from "react-spring";
+
+const config = { mass: 2, tension: 2000, friction: 200 };
+
 const useStyles = makeStyles({
 
     title: {
@@ -25,41 +34,109 @@ const useStyles = makeStyles({
         textAlign: "center",
         fontSize: "250%",
     },
-
-    image: {
-        width: "15%",
-        justifyContent: "center",
-        alignItems: "center",
-    },
     card: {
         backgroundColor: "#278be2",
-    }
+    },
 });
 
-
+const goldList = [
+    {name: 'Bloomberg', 
+    imgURL: 'Bloomberg',
+    website: 'https://www.bloomberg.com/'},
+    {name: 'Yet Analytics', 
+    imgURL: 'yet',
+    website: 'https://www.yetanalytics.com/'},
+    {name: 'Bloomberg', 
+    imgURL: 'Bloomberg',
+    website: 'https://www.bloomberg.com/'},
+]
 
 function img(url) {
     return process.env.PUBLIC_URL + '/images/' + url;
 }
 
 function SponsorItem(props) {
-    const classes = useStyles();
     return (
-        <Grid container xs={6} alignItems="center" justify="center" className={classes.image} style={{ width: "15%" }}>
-
-            <picture align="center" >
-                <a href={`${props.website}`}><img srcSet={img(`sponsor/png/${props.imgURL}.png`)} style={props.imgSytle} /></a>
-            </picture>
-        </Grid>
+        <a href={`${props.website}`}><img srcSet={img(`sponsor/png/${props.imgURL}.png`)} style={props.imgSytle} /></a>
     );
-
 }
 
+function Carousel(tier, datalist) {
+    const size = useWindowSize();
+    const sideRef = React.useRef(null);
+    const controllerRef = React.useRef(null);
+
+    const [scrollWidth, setScrollWidth] = React.useState(0);
+
+    React.useLayoutEffect(() => {
+        if (sideRef.current) {
+          let boxWidth = 0;
+    
+          [...sideRef.current.children].forEach(c => {
+            const childBox = c.getBoundingClientRect();
+            boxWidth = boxWidth + childBox.width;
+          });
+    
+          const w =
+            window.innerWidth ||
+            document.documentElement.clientWidth ||
+            document.body.clientWidth;
+    
+          const totalScrollWidth = boxWidth > w ? boxWidth - w : 0;
+    
+          setScrollWidth(totalScrollWidth);
+        }
+      }, [size.height, size.width]);
+
+    return (
+        <ParallaxProvider>
+            <Controller ref={controllerRef}>
+                <Scene
+                    duration={scrollWidth}
+                    pin
+                    triggerHook={0.25}
+                    enabled={size.width > 768}
+                >
+                    {progress => {
+                    return (
+                        <div>
+                        <div
+                            className="cnt"
+                            style={{
+                            transform: `translate3d(-${scrollWidth *
+                                progress}px, 0, 0)`
+                            }}
+                        >
+                            <div
+                            className="content"
+                            ref={sideRef}
+                            style={{ width: scrollWidth }}
+                            >
+                            <div style={{ height: "100%", width: "2480px" }}>
+                                <SponsorItem imgURL='Bloomberg' website='https://www.bloomberg.com/' imgSytle={{width: size.width * tier, maxHeight: "100%"}}/>            
+                                <SponsorItem imgURL='Googlecloud' website='https://cloud.google.com/' imgSytle={{width: size.width * tier, maxHeight: "100%"}}/>
+                                <SponsorItem imgURL='Bloomberg' website='https://www.bloomberg.com/' imgSytle={{width: size.width * tier, maxHeight: "100%"}}/>            
+                            </div>
+                            </div>
+                        </div>
+                        </div>
+                    );
+                    }}
+                </Scene>
+            </Controller>
+        </ParallaxProvider>
+    );
+}
 
 export default function Sponsors() {
 
 
     const classes = useStyles();
+
+    const gold = 0.3;
+    const sable = 0.25;
+    const blue = 0.2;
+    const starter = 0.15;
 
     return (
         <Box py={2}>
@@ -68,22 +145,15 @@ export default function Sponsors() {
                     {/*TODO material UI*/}
                     <Typography className={classes.title} variant="h4" gutterBottom>Sponsors</Typography>
                     <Typography className={classes.minititle} gutterBottom>Gold</Typography> {/* Will Style Better Later */}
-
-                     {/* Carousel Goes Here! */}
+                    {Carousel(gold, goldList)}
 
                     <Typography className={classes.minititle} gutterBottom>Sable</Typography> {/* Will Style Better Later */}
-
-                     {/* Carousel Goes Here! */}
-
+                    
                     <Typography className={classes.minititle} gutterBottom>Blue</Typography> {/* Will Style Better Later */}
-
-                     {/* Carousel Goes Here! */}
-
+                    
+                    
                     <Typography className={classes.minititle} gutterBottom>Starter</Typography> {/* Will Style Better Later */}
-
-                     {/* Carousel Goes Here! */}
-
-
+                   
 
                     {/* <Grid container spacing={0} alignItems="center" justify="center">
                     <SponsorItem imgURL='Bloomberg' website='https://www.bloomberg.com/' imgSytle={{width: '100%', maxHeight: "100%"}}/>
