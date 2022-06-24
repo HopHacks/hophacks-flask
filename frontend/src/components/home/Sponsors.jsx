eb806df9b286a0bad83c26dffd7838ba0c750319
AkhilDeo <singingbird99@gmail.com>
2022-06-24 11:36:04 -0400

    Added Animations of Logos for Sponsors Section

--- frontend/src/components/home/Sponsors.jsx
+++ frontend/src/components/home/Sponsors.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 import Box from '@material-ui/core/Box';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
@@ -9,10 +8,11 @@ import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 
 import ReactDOM from "react-dom";
-import { Controller, Scene } from "react-scrollmagic";
 import { ParallaxProvider, Parallax } from "react-scroll-parallax";
 import { useWindowSize } from "react-use";
-import { useTrail, animated } from "react-spring";
+import { motion, Variants } from 'framer-motion/dist/framer-motion'// Needs to be added to requirements.txt
+import { FormHelperText } from "@material-ui/core";
+
 
 const config = { mass: 2, tension: 2000, friction: 200 };
 
@@ -22,34 +22,32 @@ const useStyles = makeStyles({
         color: "#ffffff",
         fontFamily: "Inter",
         fontWeight: "bold",
-        // fontStyle: "italic",
-        textAlign: "left",
+        textAlign: "center",
         fontSize: "375%"
     },
     minititle: {
         color: "#ffffff",
         fontFamily: "Inter",
         fontWeight: "bold",
-        // fontStyle: "italic",
+        textAlign: "center",
+        fontSize: "300%",
+        alignItems: "center",
+        justifyItems: "center"
+    },
+    logos: {
+        color: "#ffffff",
+        fontFamily: "Inter",
+        fontWeight: "bold",
         textAlign: "center",
         fontSize: "250%",
+        alignItems: "center",
+        justifyItems: "center"
+
     },
     card: {
         backgroundColor: "#278be2",
     },
 });
-
-const goldList = [
-    {name: 'Bloomberg', 
-    imgURL: 'Bloomberg',
-    website: 'https://www.bloomberg.com/'},
-    {name: 'Yet Analytics', 
-    imgURL: 'yet',
-    website: 'https://www.yetanalytics.com/'},
-    {name: 'Bloomberg', 
-    imgURL: 'Bloomberg',
-    website: 'https://www.bloomberg.com/'},
-]
 
 function img(url) {
     return process.env.PUBLIC_URL + '/images/' + url;
@@ -61,73 +59,6 @@ function SponsorItem(props) {
     );
 }
 
-function Carousel(tier, datalist) {
-    const size = useWindowSize();
-    const sideRef = React.useRef(null);
-    const controllerRef = React.useRef(null);
-
-    const [scrollWidth, setScrollWidth] = React.useState(0);
-
-    React.useLayoutEffect(() => {
-        if (sideRef.current) {
-          let boxWidth = 0;
-    
-          [...sideRef.current.children].forEach(c => {
-            const childBox = c.getBoundingClientRect();
-            boxWidth = boxWidth + childBox.width;
-          });
-    
-          const w =
-            window.innerWidth ||
-            document.documentElement.clientWidth ||
-            document.body.clientWidth;
-    
-          const totalScrollWidth = boxWidth > w ? boxWidth - w : 0;
-    
-          setScrollWidth(totalScrollWidth);
-        }
-      }, [size.height, size.width]);
-
-    return (
-        <ParallaxProvider>
-            <Controller ref={controllerRef}>
-                <Scene
-                    duration={scrollWidth}
-                    pin
-                    triggerHook={0.25}
-                    enabled={size.width > 768}
-                >
-                    {progress => {
-                    return (
-                        <div>
-                        <div
-                            className="cnt"
-                            style={{
-                            transform: `translate3d(-${scrollWidth *
-                                progress}px, 0, 0)`
-                            }}
-                        >
-                            <div
-                            className="content"
-                            ref={sideRef}
-                            style={{ width: scrollWidth }}
-                            >
-                            <div style={{ height: "100%", width: "2480px" }}>
-                                <SponsorItem imgURL='Bloomberg' website='https://www.bloomberg.com/' imgSytle={{width: size.width * tier, maxHeight: "100%"}}/>            
-                                <SponsorItem imgURL='Googlecloud' website='https://cloud.google.com/' imgSytle={{width: size.width * tier, maxHeight: "100%"}}/>
-                                <SponsorItem imgURL='Bloomberg' website='https://www.bloomberg.com/' imgSytle={{width: size.width * tier, maxHeight: "100%"}}/>            
-                            </div>
-                            </div>
-                        </div>
-                        </div>
-                    );
-                    }}
-                </Scene>
-            </Controller>
-        </ParallaxProvider>
-    );
-}
-
 export default function Sponsors() {
 
 
@@ -135,38 +66,100 @@ export default function Sponsors() {
 
     const gold = 0.3;
     const sable = 0.25;
-    const blue = 0.2;
+    const blue = 0.15;
     const starter = 0.15;
 
+    const textAnimate = {
+        offscreen: { y: 0, opacity: 0 },
+        onscreen: {
+            y: 0,
+            opacity: 1,
+            duration: 4,
+
+            transition: {
+                type: "spring",
+                bounce: 0.0,
+                duration: 2
+            }
+        }
+    }
+
+    const imageAnimate = {
+        offscreen: { x: -750, opacity: 0 },
+        onscreen: {
+            x: 0,
+            opacity: 1,
+            duration: 3,
+            transition: {
+                type: "spring",
+                bounce: 0.0,
+                duration: 4
+            }
+        }
+    }
+    const size = useWindowSize();
+
+
     return (
-        <Box py={2}>
-            <Card className={classes.card}>
-                <CardContent>
-                    {/*TODO material UI*/}
-                    <Typography className={classes.title} variant="h4" gutterBottom>Sponsors</Typography>
-                    <Typography className={classes.minititle} gutterBottom>Gold</Typography> {/* Will Style Better Later */}
-                    {Carousel(gold, goldList)}
+        <motion.div>
+            <Box py={2}>
+                <Card className={classes.card}>
+                    <CardContent>
+                        <div>
+                            <h4 className={classes.title} gutterBottom>Sponsors</h4>
+                        </div>
+                        <div>
+                            <Typography className={classes.minititle} gutterBottom>Gold</Typography>
+                        </div>
+                        <motion.div class={classes.logos}
+                            initial={"offscreen"}
+                            whileInView={"onscreen"}
+                            variants={imageAnimate}>
+                            <span alignItems="center">
+                                <SponsorItem imgURL='Bloomberg' website='https://www.bloomberg.com/' imgSytle={{ width: size.width * gold, maxHeight: "100%", height: 150, alignItems: "center" }} />
+                                <SponsorItem imgURL='yet' website='https://www.yetanalytics.com/' imgSytle={{ width: size.width * gold, maxHeight: "100%", height: 125, alignItems: "center" }} />
+                            </span>
+                        </motion.div>
+                        <div>
+                            <Typography className={classes.minititle} gutterBottom>Sable</Typography>
+                        </div>
+                        <motion.div class={classes.logos}
+                            initial={"offscreen"}
+                            whileInView={"onscreen"}
+                            variants={imageAnimate}>
+                            <SponsorItem imgURL='apl' website='https://www.jhuapl.edu/' imgSytle={{ width: size.width * sable , maxHeight: "100%", marginTop: "5%" }} />
+                            <SponsorItem imgURL='one' website='https://www.capitalone.com/' imgSytle={{ width: size.width * sable, maxHeight: "100%", marginTop: "5%" }} />
+                            <br></br>
+                            <SponsorItem imgURL='SIEMENS' website='https://www.siemens-healthineers.com/' imgSytle={{ width: size.width * sable, maxHeight: "100%", marginTop: "7%" }} />
+                            <SponsorItem imgURL='it' website='https://it.johnshopkins.edu/' imgSytle={{ width: size.width * sable / 1.5, maxHeight: "100%", marginTop: "12%" }} />
 
-                    <Typography className={classes.minititle} gutterBottom>Sable</Typography> {/* Will Style Better Later */}
-                    
-                    <Typography className={classes.minititle} gutterBottom>Blue</Typography> {/* Will Style Better Later */}
-                    
-                    
-                    <Typography className={classes.minititle} gutterBottom>Starter</Typography> {/* Will Style Better Later */}
-                   
+                        </motion.div>
+                        <div>
+                            <Typography className={classes.minititle} gutterBottom>Blue</Typography>
+                        </div>
+                        <motion.div class={classes.logos}
+                            initial={"offscreen"}
+                            whileInView={"onscreen"}
+                            variants={imageAnimate}>
+                            <SponsorItem imgURL='ffu' website='https://ventures.jhu.edu/programs-services/fastforward-u/' imgSytle={{ width: size.width * blue * 1.5, maxHeight: "100%", marginTop: "5%" }} />
+                        </motion.div>
+                        <div>
+                            <Typography className={classes.minititle} gutterBottom>Starter</Typography>
+                        </div>
+                        <motion.div class={classes.logos}
+                            initial={"offscreen"}
+                            whileInView={"onscreen"}
+                            variants={imageAnimate}>
+                            <SponsorItem imgURL='linode' website='https://www.linode.com/' imgSytle={{ width: size.width * starter * 1.25, maxHeight: "100%", marginTop: "5%" }} />
+                            <SponsorItem imgURL='ground' website='https://www.groundcontrol.coffee/' imgSytle={{width: size.width * starter, maxHeight: "100%", marginTop: "1%"}}/>
+                            <br></br>
+                            <SponsorItem imgURL='wolfram-alpha' website='https://www.wolframalpha.com/' imgSytle={{width: size.width * starter * 2, maxHeight: "100%", marginTop: "5%"}}/>
+                            <SponsorItem imgURL='Googlecloud' website='https://cloud.google.com/' imgSytle={{width: size.width * starter * 1.75, maxHeight: "100%", marginTop: "1%"}}/>
+                        </motion.div>
+                    </CardContent>
+                </Card>
+            </Box >
+        </motion.div>
 
-                    {/* <Grid container spacing={0} alignItems="center" justify="center">
-                    <SponsorItem imgURL='Bloomberg' website='https://www.bloomberg.com/' imgSytle={{width: '100%', maxHeight: "100%"}}/>
-                    </Grid>
-                    
-                    <Grid container spacing={0} alignItems="center" justify="center">
-                    <SponsorItem imgURL='Googlecloud' website='https://cloud.google.com/' imgSytle={{width: '100%', maxHeight: "100%", marginTop: "1%"}}/>
-                    </Grid> */}
-
-
-
-                </CardContent>
-            </Card>
-        </Box>
     );
 }
