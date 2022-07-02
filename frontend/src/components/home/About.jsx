eefe9360da209384fbaafcf54c60f8ed3aa38b06
SP-Zeng <77908144+SP-Zeng@users.noreply.github.com>
2022-07-02 01:18:49 -0400

    update so the animation is more reactive

--- frontend/src/components/home/About.jsx
+++ frontend/src/components/home/About.jsx
@@ -108,9 +108,9 @@ export default function About() {
 
     const scaleRight = useTransform(scrollY, [0, 500], [2, 1]);
     const yRight = useTransform(scrollY, [0, 500], ["25vh", "0vh"]);
-    const xRight = useTransform(scrollY, [800, 100], ["15%", "-23vw"]);
+    const xRight = useTransform(scrollY, [1500, 300], ["15%", "-23vw"]);
 
-    const xLeft = useTransform(scrollY, [800,100], ["-25%", "20vw"]);
+    const xLeft = useTransform(scrollY, [1500,300], ["-25%", "20vw"]);
 
     const xLeft2 = useTransform(scrollY, [1500,300], ["5%", "100vw"]);
  
