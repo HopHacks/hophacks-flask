6243e2141a699d735ead8cb39322b3abc5dcbb21
SP-Zeng <77908144+SP-Zeng@users.noreply.github.com>
2022-07-08 22:16:45 -0400

    include schedule change

--- frontend/src/components/home/Schedule.jsx
+++ frontend/src/components/home/Schedule.jsx
@@ -42,6 +42,11 @@ const useStyles = makeStyles({
   font: {
     fontFamily: "Inter",
     color: "#000000",
+  },
+  divFont: {
+    fontFamily: "Inter",
+    color: "#000000",
+    height: "30px"
   }
 });
 
@@ -66,7 +71,7 @@ export default function Schedule() {
           <Typography className={classes.title} variant="h4" gutterBottom>
             Schedule
           </Typography>
-          <div className={classes.font}>All times in EDT (GMT-4)</div>
+          <div className={classes.divFont}>All times in EDT (GMT-4)</div>
 
           <TableContainer component={Paper} style={{ width: "100%", backgroundColor:"#eef7ff"}} align="center" sx={{ maxHeight: 440 }}>
 
@@ -80,9 +85,9 @@ export default function Schedule() {
               className={classes.schedule}
               variant="fullWidth"
             >
-              <Tab label="Fri September 17th" value="fri" className={classes.font}/>
-              <Tab label="Sat September 18th" value="sat" className={classes.font}/>
-              <Tab label="Sun September 19th" value="sun" className={classes.font}/>
+              <Tab label="Fri September 16th" value="fri" className={classes.font}/>
+              <Tab label="Sat September 17th" value="sat" className={classes.font}/>
+              <Tab label="Sun September 18th" value="sun" className={classes.font}/>
             </Tabs>
 
             <Table className={classes.table} aria-label="simple table" sx={{
@@ -92,113 +97,116 @@ export default function Schedule() {
                 <TableRow>
                   <TableCell className={classes.font}>Time</TableCell>
                   <TableCell  className={classes.font}>Event</TableCell>
-                  <TableCell className={classes.font}>Location/Link</TableCell>
+                  <TableCell className={classes.font}>Location</TableCell>
                 </TableRow>
               </TableHead>
               {day === "fri" && <TableBody className={classes.body}>
                 <TableRow className={classes.font}>
-                  <TableCell width="40%" className={classes.font}>5:00 PM</TableCell>
+                  <TableCell width="40%" className={classes.font}>6:30 PM</TableCell>
                   <TableCell className={classes.font}>Check-in Begins </TableCell>
-                  <TableCell align="left" className={classes.font}></TableCell>
+                  <TableCell align="left" className={classes.font}>Hodson Hall</TableCell>
                 </TableRow>
+
                 <TableRow className={classes.bodycolor}>
-                  <TableCell width="40%" className={classes.font}>8:00 PM</TableCell>
-                  <TableCell className={classes.font}>Opening Ceremony</TableCell>
-                  <TableCell align="left" className={classes.font}><a href="https://twitch.tv/hophacks">Twitch</a></TableCell>
+                  <TableCell width="40%" className={classes.font}>6:30 PM</TableCell>
+                  <TableCell className={classes.font}>Dinner</TableCell>
+                  <TableCell align="left" className={classes.font}>Hodson Hall</TableCell>
                 </TableRow>
+
+                <TableRow className={classes.bodycolor}>
+                  <TableCell width="40%" className={classes.font}>7:30 PM</TableCell>
+                  <TableCell className={classes.font}>Both Buses Arrive</TableCell>
+                  <TableCell align="left" className={classes.font}>Mason Hall Loop</TableCell>
+                </TableRow>
+
                 <TableRow className={classes.bodycolor}>
                   <TableCell width="40%" className={classes.font}>9:00 PM</TableCell>
                   <TableCell className={classes.font}>Team Building</TableCell>
-                  <TableCell align="left" className={classes.font}></TableCell>
+                  <TableCell align="left" className={classes.font}>Hodson Hall</TableCell>
                 </TableRow>
 
                 <TableRow className={classes.bodycolor}>
                   <TableCell width="40%" className={classes.font}>9:00 PM</TableCell>
                   <TableCell className={classes.font}> Sponsor Hall Opens & Hacking Begins!</TableCell>
-                  <TableCell align="left" className={classes.font}></TableCell>
+                  <TableCell align="left" className={classes.font}>Hodson Hall</TableCell>
                 </TableRow>
               </TableBody>}
 
               {day === "sat" && <TableBody className={classes.body}>
                 <TableRow className={classes.bodycolor}>
                   <TableCell width="40%" className={classes.font}>9:00 AM</TableCell>
-                  <TableCell className={classes.font}>Devpost Checkpoint & Check-in Ends</TableCell>
-                  <TableCell align="left" className={classes.font}></TableCell>
-                </TableRow>
-
-                <TableRow className={classes.bodycolor}>
-                  <TableCell width="40%" className={classes.font}>11:00 AM</TableCell>
-                  <TableCell className={classes.font}>MLH Event: Capture the Flag Presented by US Air Force</TableCell>
-                  <TableCell align="left" className={classes.font}>Discord Livestream</TableCell>
-                </TableRow>
-
-                <TableRow className={classes.bodycolor}>
-                  <TableCell width="40%" className={classes.font}>11:00 AM</TableCell>
-                  <TableCell className={classes.font}>Fast Forward U Workshop: Create Your Own Landing Page!</TableCell>
-                  <TableCell align="left" className={classes.font}><a href="https://zoom.us/my/startupxp">Zoom Link</a></TableCell>
+                  <TableCell className={classes.font}>Breakfast</TableCell>
+                  <TableCell align="left" className={classes.font}>Hodson Hall</TableCell>
                 </TableRow>
 
                 <TableRow className={classes.bodycolor}>
                   <TableCell width="40%" className={classes.font}>1:00 PM</TableCell>
-                  <TableCell className={classes.font}>Bloomberg Tech Talk: Detecting Gender Bias in Software UI design</TableCell>
-                  <TableCell align="left" className={classes.font}><a href="https://bloomberg.zoom.us/j/94547754929?pwd=OHVzQWFtNFRHRUhuWmFUU2Z6V1FMdz09">Zoom Link</a></TableCell>
+                  <TableCell className={classes.font}>Lunch</TableCell>
+                  <TableCell align="left" className={classes.font}>Hodson Hall</TableCell>
                 </TableRow>
 
                 <TableRow className={classes.bodycolor}>
-                  <TableCell width="40%" className={classes.font}>3:00 PM</TableCell>
-                  <TableCell className={classes.font}>Yet Analytics Workshop: IEEE Learning Technology Standards Committee Data Standards.</TableCell>
-                  <TableCell align="left" className={classes.font}>TBD</TableCell>
-                </TableRow>
-
-                <TableRow className={classes.bodycolor}>
-                  <TableCell width="40%" className={classes.font}>4:00 PM</TableCell>
-                  <TableCell className={classes.font}>HopHacks & HopAI Workshop: Generative Adversarial Networks</TableCell>
-                  <TableCell align="left" className={classes.font}><a href="https://jhubluejays.zoom.us/j/95919110259?pwd=cWRxZjVLT0ZZbXQvZ3NhZWlCWDVwQT09">Zoom Link</a></TableCell>
-                </TableRow>
-
-                <TableRow className={classes.bodycolor}>
-                  <TableCell width="40%" className={classes.font}>8:00 PM</TableCell>
-                  <TableCell className={classes.font}>MLH Event: Bob Ross MS Paint</TableCell>
-                  <TableCell align="left" className={classes.font}>Discord Livestream</TableCell>
+                  <TableCell width="40%" className={classes.font}>6:00 PM</TableCell>
+                  <TableCell className={classes.font}>Dinner</TableCell>
+                  <TableCell align="left" className={classes.font}>Hodson Hall</TableCell>
                 </TableRow>
 
                 <TableRow className={classes.bodycolor}>
                   <TableCell width="40%" className={classes.font}>9:00 PM</TableCell>
-                  <TableCell className={classes.font}>Devpost Checkpoint</TableCell>
-                  <TableCell align="left" className={classes.font}></TableCell>
-                </TableRow>
-
-                <TableRow className={classes.bodycolor}>
-                  <TableCell width="40%" className={classes.font}>10:00 PM</TableCell>
-                  <TableCell className={classes.font}>echoAR Workshop: How to Build a Cloud-Connected AR/VR App in 15 Minutes or Less</TableCell>
-                  <TableCell align="left" className={classes.font}><a href="https://twitch.tv/hophacks">Twitch Link</a></TableCell>
+                  <TableCell className={classes.font}>Fun (video game tournament, movie night, etc)</TableCell>
+                  <TableCell align="left" className={classes.font}>Hodson Hall</TableCell>
                 </TableRow>
               </TableBody>}
-
               {day === "sun" &&<TableBody className={classes.body}>
                 <TableRow className={classes.bodycolor}>
-                  <TableCell width="40%" className={classes.font}>8:45 AM</TableCell>
+                  <TableCell width="40%" className={classes.font}>8:30 AM</TableCell>
                   <TableCell className={classes.font}>Submissions Due</TableCell>
-                  <TableCell align="left" className={classes.font}></TableCell>
+                  <TableCell align="left" className={classes.font}>Devpost</TableCell>
                 </TableRow>
 
                 <TableRow className={classes.bodycolor}>
-                  <TableCell width="40%" className={classes.font}>9:30 AM</TableCell>
+                  <TableCell width="40%" className={classes.font}>9:00 AM</TableCell>
+                  <TableCell className={classes.font}>Breakfast</TableCell>
+                  <TableCell align="left" className={classes.font}>Hodson Hall</TableCell>
+                </TableRow>
+
+                <TableRow className={classes.bodycolor}>
+                  <TableCell width="40%" className={classes.font}>10:00 AM</TableCell>
                   <TableCell className={classes.font}>Presentations Fair</TableCell>
-                  <TableCell align="left" className={classes.font}></TableCell>
+                  <TableCell align="left" className={classes.font}>Hodson Hall</TableCell>
+                </TableRow>
+
+                <TableRow className={classes.bodycolor}>
+                  <TableCell width="40%" className={classes.font}>12:00 PM</TableCell>
+                  <TableCell className={classes.font}>Lunch</TableCell>
+                  <TableCell align="left" className={classes.font}>Hodson Hall</TableCell>
                 </TableRow>
 
                 <TableRow className={classes.bodycolor}>
                   <TableCell width="40%" className={classes.font}>12:30 PM</TableCell>
                   <TableCell className={classes.font}>Top 10 Demos & Judging</TableCell>
-                  <TableCell align="left" className={classes.font}><a href="https://twitch.tv/hophacks">Twitch Link</a></TableCell>
+                  <TableCell align="left" className={classes.font}>Hodson Hall</TableCell>
+                </TableRow>
+                          
+                <TableRow className={classes.bodycolor}>
+                  <TableCell width="40%" className={classes.font}>1:45 PM</TableCell>
+                  <TableCell className={classes.font}>Awards & Closing Ceremony</TableCell>
+                  <TableCell align="left" className={classes.font}>Hodson Hall</TableCell>
                 </TableRow>
 
+                
                 <TableRow className={classes.bodycolor}>
-                  <TableCell width="40%" className={classes.font}>2:30 PM</TableCell>
-                  <TableCell className={classes.font}>Awards & Closing Ceremony</TableCell>
-                  <TableCell align="left" className={classes.font}><a href="https://twitch.tv/hophacks">Twitch Link</a></TableCell>
+                  <TableCell width="40%" className={classes.font}>2:45 PM</TableCell>
+                  <TableCell className={classes.font}>Buses Load</TableCell>
+                  <TableCell align="left" className={classes.font}>Mason Hall Loop</TableCell>
                 </TableRow>
+                
+                <TableRow className={classes.bodycolor}>
+                  <TableCell width="40%" className={classes.font}>3:00 PM</TableCell>
+                  <TableCell className={classes.font}>Buses Leave</TableCell>
+                  <TableCell align="left" className={classes.font}>Mason Hall Loop</TableCell>
+                </TableRow>
+
               </TableBody>}
             </Table>
           </TableContainer>
