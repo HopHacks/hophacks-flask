98b03af09450c52adb93a8bde6739ae20c166108
AkhilDeo <singingbird99@gmail.com>
2022-07-25 22:38:47 -0400

    Added home changes

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -52,7 +52,7 @@ const useStyles = makeStyles({
     blank: {
         padding: "30px",
         backgroundColor: "#c8e7fa"
-    }
+    },
 }
 );
 
@@ -70,7 +70,7 @@ export default function Home() {
         height: window.innerHeight
     });
 
-    
+
 
     useEffect(() => {
         setTimeout(() => {
@@ -79,16 +79,16 @@ export default function Home() {
 
         function handleWindowResize() {
             setWindowSize({
-              width: window.innerWidth,
-              height: window.innerHeight
+                width: window.innerWidth,
+                height: window.innerHeight
             });
         }
 
         window.addEventListener('resize', handleWindowResize);
-    
+
         return () => {
-          window.removeEventListener('resize', handleWindowResize);
-          
+            window.removeEventListener('resize', handleWindowResize);
+
         };
 
     }, [])
@@ -109,40 +109,40 @@ export default function Home() {
     if (window.innerWidth <= 650) {
         return (
             <div>
-            
-            <div className={classes.gradient}>
-                <div>
-                    <a id="mlh-trust-badge"
-                        style={{ 'display': 'block', 'maxWidth': '100px', 'minWidth': '60px', 'position': 'fixed', 'right': '30px', 'top': '0', 'width': '10%', 'zIndex': '10000' }}
-                        href="https://mlh.io/seasons/2022/events?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2022-season&utm_content=gray"
-                        target="_blank">
-                        <img src="https://s3.amazonaws.com/logged-assets/trust-badge/2022/mlh-trust-badge-2022-gray.svg" alt="Major League Hacking 2022 Hackathon Season" style={{ "width": "100%" }}></img>
-                    </a>
+
+                <div className={classes.gradient}>
                     <div>
-                        {loading
-                        ?  (<LoadingAnimation/>)
-                        : (
-                        <>
-                            <Container fixed>
-                                <Cover/>
-                                <AboutTransition />
-                                <About />
-                                <Schedule />
-                                <Prizes />
-                                <Sponsors />
-                                <Faq />
-                                <span STYLE="font-size:300%" >&nbsp;&nbsp;</span>
-                                {/* <Team /> */}
-                                
-                            </Container>
-                            <Footer/>
-                            
-                        </>
-                        )
-                        }
+                        <a id="mlh-trust-badge"
+                            style={{ 'display': 'block', 'maxWidth': '100px', 'minWidth': '60px', 'position': 'fixed', 'right': '30px', 'top': '0', 'width': '10%', 'zIndex': '10000' }}
+                            href="https://mlh.io/seasons/2022/events?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2022-season&utm_content=gray"
+                            target="_blank">
+                            <img src="https://s3.amazonaws.com/logged-assets/trust-badge/2022/mlh-trust-badge-2022-gray.svg" alt="Major League Hacking 2022 Hackathon Season" style={{ "width": "100%" }}></img>
+                        </a>
+                        <div>
+                            {loading
+                                ? (<LoadingAnimation />)
+                                : (
+                                    <>
+                                        <Container fixed>
+                                            <Cover />
+                                            <AboutTransition />
+                                            <About />
+                                            <Schedule />
+                                            <Prizes />
+                                            <Sponsors />
+                                            <Faq />
+                                            <span STYLE="font-size:300%" >&nbsp;&nbsp;</span>
+                                            {/* <Team /> */}
+
+                                        </Container>
+                                        <Footer />
+
+                                    </>
+                                )
+                            }
+                        </div>
                     </div>
                 </div>
-            </div>
             </div>
         );
     } else {
@@ -155,15 +155,15 @@ export default function Home() {
                         target="_blank">
                         <img src="https://s3.amazonaws.com/logged-assets/trust-badge/2022/mlh-trust-badge-2022-gray.svg" alt="Major League Hacking 2022 Hackathon Season" style={{ "width": "100%" }}></img>
                     </a>
-    
+
                     <div>
-                        {loading ? (<LoadingAnimation/>)
-                        : ( <Container fixed>
+                        {loading ? (<LoadingAnimation />)
+                            : (<Container fixed>
                                 <motion.div class={classes.logos}
                                     initial={"onscreen"}
                                     whileInView={"onscreen"}
                                     variants={CoverAnimate}>
-                                    <Cover/>
+                                    <Cover />
                                 </motion.div>
                                 <AboutTransition />
                                 <About />
@@ -173,7 +173,7 @@ export default function Home() {
                                 <Faq />
                                 <span STYLE="font-size:300%" >&nbsp;&nbsp;</span>
                                 {/* <Team /> */}
-                                <Footer/>
+                                <Footer />
                             </Container>)
                         }
                     </div>
@@ -181,6 +181,6 @@ export default function Home() {
             </div>
         );
     }
-    
+
 }
 
