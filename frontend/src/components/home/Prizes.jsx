e4fe4dd5fddd4c95d340eb997332953eb4521d66
SP-Zeng <77908144+SP-Zeng@users.noreply.github.com>
2022-07-27 01:58:39 -0400

    animation

--- frontend/src/components/home/Prizes.jsx
+++ frontend/src/components/home/Prizes.jsx
@@ -20,89 +20,18 @@ const useStyles = makeStyles({
         fontSize: "375%"
     },
 
-    firstTitle: {
-        fontFamily: "Inter",
-        position: 'absolute',
-        top: "29%",
-        left: "25%",
-        color: "black",
-        // fontSize: "1.4vw",
-    },
-
-    firstSubTitle: {
-        fontFamily: "Inter",
-        position: 'absolute',
-        top: "33%",
-        left: "27%",
-        color: "black",
-        // fontSize: "1.2vw",
-    },
-  
-
-
-    // secondTitle: {
-    //     fontFamily: "Inter",
-    //     position: 'relative',
-    //     top: 160,
-    //     bottom: 0,
-    //     left: 265,
-    //     right: 0,
-    //     color: "white",
-    // },
-    // secondSubTitle: {
-    //     fontFamily: "Inter",
-    //     position: 'relative',
-    //     top: 160,
-    //     bottom: 0,
-    //     left: 295,
-    //     right: 0,
-    //     color: "white",
-    // },
-
-    // thirdTitle: {
-    //     fontFamily: "Inter",
-    //     position: 'relative',
-    //     top: -245,
-    //     bottom: 0,
-    //     left: 845,
-    //     right: 0,
-    //     color: "white",
-    // },
-
-    // thirdSubTitle: {
-    //     fontFamily: "Inter",
-    //     position: 'relative',
-    //     top: -245,
-    //     bottom: 0,
-    //     left: 870,
-    //     right: 0,
-    //     color: "white",
-    // },
-
     color: {
         backgroundColor: "#d1e9ff",
         
     },
 
-    margin: {
-        minWidth:"100px",
-        width: "15%",
-    },
-
-    margin2: {
-        minWidth:"100px",
-        width: "20%",
-    },
-
-    margin3: {
-        width: "50%",
-    },
     birds: {
         backgroundColor: "transparent",
         // width: "80%",
         // left: "100px",
         position: "relative"
     },
+
     card: {
         position: "relative",
         border: "none", 
@@ -110,9 +39,6 @@ const useStyles = makeStyles({
         backgroundColor: "transparent",
         left: "-22px"
     },
-
-    
-
 
     chatBox1: {
         backgroundColor: "transparent",
@@ -200,84 +126,39 @@ export default function Prizes() {
             </Grid>              
             </Grid>
             <Card className={classes.card}>
-                <CardMedia
-                    component="img"
-                    className={classes.birds}
-                    image={img('Birds.png')}
-                />
                 <motion.div
-                initial={"offscreen"}
-                whileInView={"onscreen"}
-                variants={imageAnimate}>
-                <div>
+                    initial={"offscreen"}
+                    whileInView={"onscreen"}
+                    variants={imageAnimate}>
                     <CardMedia
                         component="img"
-                        image={img('2nd_chatbox.png')}
-                        className={classes.chatBox1}
+                        className={classes.birds}
+                        image={img('Birds.png')}
                     />
-                </div>
-                <div>
-                    <CardMedia
-                        component="img"
-                        image={img('1st_chatbox.png')}
-                        className={classes.chatBox2}
-                    />
-                </div>
-                <div>
-                    <CardMedia
-                        component="img"
-                        image={img('3rd_chatbox.png')}
-                        className={classes.chatBox3}
-                    />
-                </div>  
+                
+                    <div>
+                        <CardMedia
+                            component="img"
+                            image={img('2nd_chatbox.png')}
+                            className={classes.chatBox1}
+                        />
+                    </div>
+                    <div>
+                        <CardMedia
+                            component="img"
+                            image={img('1st_chatbox.png')}
+                            className={classes.chatBox2}
+                        />
+                    </div>
+                    <div>
+                        <CardMedia
+                            component="img"
+                            image={img('3rd_chatbox.png')}
+                            className={classes.chatBox3}
+                        />
+                    </div>  
                 </motion.div>                   
             </Card>
-            {/* <motion.div
-                initial={"offscreen"}
-                whileInView={"onscreen"}
-                variants={imageAnimate}>
-                <div>
-                    <CardMedia
-                        component="img"
-                        className={classes.chatBox1}
-                        image={img('chatbox_above_birds.png')}
-                    />
-                    <Typography variant="h5" className={classes.secondTitle}  gutterBottom>
-                        <b>2nd Place </b>
-                        </Typography>
-                    <Typography variant="h6" className={classes.secondSubTitle} >
-                        $512
-                    </Typography>
-                </div>  
-            
-                <div> 
-                    <CardMedia
-                        component="img"
-                        className={classes.chatBox2}
-                        image={img('chatbox_above_birds.png')}
-                    />
-                    <Typography variant="h5" className={classes.firstTitle} align="center" gutterBottom>
-                        <b>1st Place </b>
-                        </Typography>
-                    <Typography variant="h6" className={classes.firstTitle} align="center">
-                        $1024
-                    </Typography>
-                </div>
-                <div>
-                    <CardMedia
-                        component="img"
-                        className={classes.chatBox3}
-                        image={img('chatbox_above_birds.png')}
-                    />
-                    <Typography variant="h5" className={classes.thirdTitle}  gutterBottom>
-                        <b>3rd Place </b>
-                        </Typography>
-                    <Typography variant="h6" className={classes.thirdSubTitle} >
-                        $256
-                    </Typography>
-                </div> 
-            </motion.div> */}
-            
         </Box>
     );
 }
