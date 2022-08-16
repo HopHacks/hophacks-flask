4b6759d1681e0a916170bdb1b8de2356b4b1ae81
Josephine Deng <jojod_27@yahoo.com>
2022-08-16 00:00:40 -0400

    footer is centered

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -13,42 +13,41 @@ const Footer = function Footer(props) {
     return (
         <Box pt={5} bgcolor="#FFC0CB" color="white">
         <Container maxWidth="lg">
-            <Grid container spacing={5}>
-
-                <Grid item xs={12} sm={2}>
-                    <img src={img('bluejay-icon.png')} style={{ width: "100px"}} />
+            <Grid container spacing={3}>
+                <Grid item xs={12} sm={4}>
+                    <Grid container direction="column" alignItems="center" justifyContent="center">
+                        <img src={img('bluejay-icon.png')} style={{ width: "100px" }} />
+                    </Grid>
                 </Grid>
-
-                <Grid item xs={6} sm={3}>
-                    
-                    <Grid container>
+                <Grid item xs={12} sm={4}>
+                    <Grid container direction="column" alignItems="center" justifyContent="center">
                         <Typography style={{fontFamily: "Inter", fontSize: 18}}>
                         <a href="mailto:hophacks2022@gmail.com" style = {{textDecoration: 'none', color:'white' }}>hophacks2022@gmail.com</a>
                         </Typography>
                     </Grid>
-                    <Grid container><Typography style={{fontFamily: "Inter", fontSize: 18}}>Malone Hall</Typography></Grid>
-                    <Grid container><Typography style={{fontFamily: "Inter", fontSize: 18}}>Johns Hopkins University</Typography></Grid>
-                    <Grid container>
+                    <Grid container direction="column" alignItems="center" justifyContent="center"><Typography style={{fontFamily: "Inter", fontSize: 18}}>Malone Hall</Typography></Grid>
+                    <Grid container direction="column" alignItems="center" justifyContent="center"><Typography style={{fontFamily: "Inter", fontSize: 18}}>Johns Hopkins University</Typography></Grid>
+                    <Grid container direction="column" alignItems="center" justifyContent="center">
                         <Typography style={{fontFamily: "Inter", fontSize: 18}}>
                         <a href="https://static.mlh.io/docs/mlh-code-of-conduct.pdf" style = {{textDecoration: 'none', color:'#51A5F7' }}>MLH Code of Conduct</a>
                         </Typography>
                     </Grid>
-                </Grid>
-                <Grid item xs={6} sm={2}>
-                    <Grid container><Typography style={{fontFamily: "Inter", fontSize: 20}}>Follow Us</Typography></Grid>
-                    <Grid container>
+                </Grid>           
+                <Grid item xs={12} sm={4}>
+                    <Grid container direction="column" alignItems="center" justifyContent="center"><Typography style={{fontFamily: "Inter", fontSize: 20}}>Follow Us</Typography></Grid>
+                    <Grid container direction="column" alignItems="center" justifyContent="center">
                         <Grid item>
                             <a href="https://www.facebook.com/HopHacks" title="Facebook">
                                 <img src={img('fb-icon.png')} style={{ width: "40px", margin: "10px 5px 5px" }} alt="fb-icon" />
                             </a>
-                        </Grid>
-                        <Grid item>
-                            <a href="https://www.linkedin.com/company/hophacks/" title="Facebook">
+                            <a href="https://www.linkedin.com/company/hophacks/" title="LinkedIn">
                                 <img src={img('linkedin-icon.png')} style={{ width: "40px", margin: "10px 5px 5px" }} alt="linkedin-icon" />
                             </a>
                         </Grid>
                     </Grid>
                 </Grid>
+
+                <Grid item xs={12}></Grid>
             </Grid>
         </Container>
         </Box>  
