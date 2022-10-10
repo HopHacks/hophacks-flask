28f2133962bf200ff793783a3956923afc361d54
Josephine Deng <jodeng256@gmail.com>
2022-10-10 17:06:05 -0400

    add 'join the team' button to cover page

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,180 +1,191 @@
 import React from "react";
-import Container from '@material-ui/core/Container';
-import { makeStyles } from '@material-ui/core/styles';
-import Team from './home/Team';
+import Container from "@material-ui/core/Container";
+import { makeStyles } from "@material-ui/core/styles";
+import Team from "./home/Team";
 import Sponsors from "./home/Sponsors";
 import Prizes from "./home/Prizes";
 import Schedule from "./home/Schedule";
-import About from "./home/About"
-import Faq from "./home/Faq"
+import About from "./home/About";
+import Faq from "./home/Faq";
 import Cover from "./home/Cover";
 import LoadingAnimation from "./home/LoadingAnimation";
 import { useState, useEffect } from "react";
 import AboutTransition from "./home/AboutTransition";
-import Footer from "./Footer"
-import { motion, useScroll } from 'framer-motion/dist/framer-motion'// Needs to be added to requirements.txt
-
+import Footer from "./Footer";
+import { motion, useScroll } from "framer-motion/dist/framer-motion"; // Needs to be added to requirements.txt
 
 const useStyles = makeStyles({
-    logo: {
-        top: '25%',
-        width: '60vw', // This is centered, 20 - 60 - 20
-        left: '20vw',
-        position: 'absolute',
-        textAlign: 'center',
-    },
-    margin: {
-        marginBottom: "13px",
-    },
-    color: {
-        //backgroundColor: "#2195ea",
-        backgroundColor: "#376efa",
+  logo: {
+    top: "25%",
+    width: "60vw", // This is centered, 20 - 60 - 20
+    left: "20vw",
+    position: "absolute",
+    textAlign: "center",
+  },
+  margin: {
+    marginBottom: "13px",
+  },
+  color: {
+    //backgroundColor: "#2195ea",
+    backgroundColor: "#376efa",
+  },
+  colorBackground: {
+    backgroundColor: "#376eea",
+  },
+  title: {
+    color: "#ffffff",
+    fontFamily: "VCR OSD Mono",
+  },
 
+  button: {
+    backgroundColor: "#FFFFFF",
+    color: "#c8e7fa",
+    width: "50%",
+    minHeight: "50px",
+    border: "4px solid",
+    "&:hover": {
+      backgroundColor: "#c8e7fa",
     },
-    colorBackground: {
-        backgroundColor: "#376eea",
-    },
-    title: {
-        color: "#ffffff",
-        fontFamily: "VCR OSD Mono",
-    },
+  },
+  gradient: {
+    backgroundImage: "linear-gradient(#15ABFB, #f179c8)",
+  },
 
-    button: {
-        backgroundColor: "#FFFFFF", color: "#c8e7fa", width: "50%", minHeight: "50px", border: "4px solid",
-        "&:hover": {
-            backgroundColor: "#c8e7fa",
-        }
-    },
-    gradient: {
-        backgroundImage: "linear-gradient(#15ABFB, #f179c8)",
-    },
-
-    blank: {
-        padding: "30px",
-        backgroundColor: "#c8e7fa"
-    },
-}
-);
+  blank: {
+    padding: "30px",
+    backgroundColor: "#c8e7fa",
+  },
+});
 
 export default function Home() {
-    const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
 
-    const classes = useStyles();
+  const classes = useStyles();
 
-    function img(url) {
-        return process.env.PUBLIC_URL + '/images/' + url;
-    }
+  function img(url) {
+    return process.env.PUBLIC_URL + "/images/" + url;
+  }
 
-    const [windowSize, setWindowSize] = useState({
+  const [windowSize, setWindowSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+
+    function handleWindowResize() {
+      setWindowSize({
         width: window.innerWidth,
-        height: window.innerHeight
-    });
-
-
-
-    useEffect(() => {
-        setTimeout(() => {
-            setLoading(false)
-        }, 2000);
-
-        function handleWindowResize() {
-            setWindowSize({
-                width: window.innerWidth,
-                height: window.innerHeight
-            });
-        }
-
-        window.addEventListener('resize', handleWindowResize);
-
-        return () => {
-            window.removeEventListener('resize', handleWindowResize);
-
-        };
-
-    }, [])
-
-    const CoverAnimate = {
-        offscreen: { y: 0, opacity: 0.7 },
-        onscreen: {
-            y: 0,
-            opacity: 1,
-            transition: {
-                type: "spring",
-                bounce: 0.0
-            }
-        }
+        height: window.innerHeight,
+      });
     }
 
-    if (window.innerWidth <= 650) {
-        return (            
-            <div className={classes.gradient}>
-                <div>
-                    <div id="parallax" className="parallax">
-                        <div className="parallax-body">
-                            <main className="site-wrapper">
-                                <div className="content">
-                                    <div className="slide-wrapper">
-                                        <div className="slide-item">
-                                            <img src={img("team-page.png")} className="slide-item__image"></img>
-                                        </div>
-                                        <div className="slide-item">
-                                            <img src={img("transparent.png")} className="slide-item__image"></img>
-                                        </div>
-                                    </div>
-                                </div>
-                            </main>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        );
-    } else {
-        return (
-            <div className={classes.gradient}>
-                <div>
-                    <div id="parallax" className="parallax">
-                        <div className="parallax-body">
-                            <main className="site-wrapper">
-                                <div className="content">
-                                    <div className="slide-wrapper">
-                                        <div className="slide-item">
-                                            <img src={img("cover2.png")} className="slide-item__image"></img>
-                                        </div>
-                                        <div className="slide-item">
-                                            <img src={img("transparent.png")} className="slide-item__image"></img>
-                                        </div>
-                                    </div>
-                                </div>
-                            </main>
-                        </div>
-                    </div>
-    
-                    <div>
-                        {loading ? (<LoadingAnimation />)
-                            : (<Container fixed>
-                                <motion.div class={classes.logos}
-                                    initial={"onscreen"}
-                                    whileInView={"offscreen"}
-                                    variants={CoverAnimate}
-                                    viewport={{ once: false }}>
-                                    <Cover />
-                                </motion.div>
-                                <AboutTransition />
-                                <About />
-                                <Schedule />
-                                <Prizes />
-                                <Sponsors />
-                                <Faq />
-                                <span STYLE="font-size:300%" >&nbsp;&nbsp;</span>
-                                {/* <Team /> */}
-                                <Footer />
-                            </Container>)
-                        }
-                    </div>
-                </div>
-            </div>
-        );
-    }
+    window.addEventListener("resize", handleWindowResize);
 
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    };
+  }, []);
+
+  const CoverAnimate = {
+    offscreen: { y: 0, opacity: 1 },
+    onscreen: {
+      y: 0,
+      opacity: 0.7,
+      transition: {
+        type: "spring",
+        bounce: 0.0,
+      },
+    },
+  };
+
+  if (window.innerWidth <= 650) {
+    return (
+      <div className={classes.gradient}>
+        <div>
+          <div id="parallax" className="parallax">
+            <div className="parallax-body">
+              <main className="site-wrapper">
+                <div className="content">
+                  <div className="slide-wrapper">
+                    <div className="slide-item">
+                      <img
+                        src={img("team-page.png")}
+                        className="slide-item__image"
+                      ></img>
+                    </div>
+                    <div className="slide-item">
+                      <img
+                        src={img("transparent.png")}
+                        className="slide-item__image"
+                      ></img>
+                    </div>
+                  </div>
+                </div>
+              </main>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className={classes.gradient}>
+        <div>
+          <div id="parallax" className="parallax">
+            <div className="parallax-body">
+              <main className="site-wrapper">
+                <div className="content">
+                  <div className="slide-wrapper">
+                    <div className="slide-item">
+                      <img
+                        src={img("cover2.png")}
+                        className="slide-item__image"
+                      ></img>
+                    </div>
+                    <div className="slide-item">
+                      <img
+                        src={img("transparent.png")}
+                        className="slide-item__image"
+                      ></img>
+                    </div>
+                  </div>
+                </div>
+              </main>
+            </div>
+          </div>
+
+          <div>
+            {loading ? (
+              <LoadingAnimation />
+            ) : (
+              <Container fixed>
+                <motion.div
+                  class={classes.logos}
+                  initial={"onscreen"}
+                  whileInView={"offscreen"}
+                  variants={CoverAnimate}
+                  viewport={{ once: false }}
+                >
+                  <Cover />
+                </motion.div>
+                <AboutTransition />
+                <About />
+                <Schedule />
+                <Prizes />
+                <Sponsors />
+                <Faq />
+                <span STYLE="font-size:300%">&nbsp;&nbsp;</span>
+                {/* <Team /> */}
+                <Footer />
+              </Container>
+            )}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-
--- frontend/src/components/home/Cover.jsx
+++ frontend/src/components/home/Cover.jsx
@@ -1,43 +1,57 @@
-import React, { useState, useEffect } from 'react'
-import { makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import { motion, useScroll } from 'framer-motion/dist/framer-motion'// Needs to be added to requirements.txt
-
-
+import React, { useState, useEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import { motion, useScroll } from "framer-motion/dist/framer-motion"; // Needs to be added to requirements.txt
 
 const useStyles = makeStyles({});
 
 function img(url) {
-    return process.env.PUBLIC_URL + '/images/' + url;
+  return process.env.PUBLIC_URL + "/images/" + url;
 }
 
-
-
 export default function Cover() {
-    const [imageLoading, setImageLoading] = useState(true);
-    const [pulsing, setPulsing] = useState(true);
+  const [imageLoading, setImageLoading] = useState(true);
+  const [pulsing, setPulsing] = useState(true);
 
-    const imageLoaded = () => {
-        setImageLoading(false);
-        setTimeout(() => setPulsing(false), 600);
-    };
+  const imageLoaded = () => {
+    setImageLoading(false);
+    setTimeout(() => setPulsing(false), 600);
+  };
 
-
-    const classes = useStyles();
-    return (
-        <div>
-            <div>
-                <img src={img('cover2.png')} style={{ position: "absolute", top: "0%", left: "0%", width: "100%", height: "100%" }} />
-                <Button onClick={() => {
-                    window.location = "/register";
-                }}
-                    color="inherit"
-                    style={{ position: "absolute", top: "75%", left: "39.50%", width: "20%", height: "10%" }}
-                >
-                    <img src={img('../register-button.png')}  style = {{width: "150%", height: "150%"}} />
-                </Button>
-            </div>
+  const classes = useStyles();
+  return (
+    <div>
+      <div>
+        <img
+          src={img("cover2.png")}
+          style={{
+            position: "absolute",
+            top: "0%",
+            left: "0%",
+            width: "100%",
+            height: "100%",
+          }}
+        />
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Button
+            style={{
+              position: "absolute",
+              top: "70%",
+              width: "25%",
+            }}
+          >
+            <a href="https://tinyurl.com/hophacksapp" target="_blank">
+              <img src={img("join_the_team.png")} style={{ width: "100%" }} />
+            </a>
+          </Button>
         </div>
-
-    );
+      </div>
+    </div>
+  );
 }
