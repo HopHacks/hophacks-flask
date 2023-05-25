f63a346a76dd73aba7b9feed0c61a6682457a67d
nishikarp <nishikarp@gmail.com>
2023-05-24 20:11:04 -0400

    landing page centered while zoom changes

--- frontend/src/components/home/Cover.jsx
+++ frontend/src/components/home/Cover.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-// import Button from '@material-ui/core/Button';
+//import Button from '@material-ui/core/Button';
 //import { motion, useScroll } from "framer-motion/dist/framer-motion"; // Needs to be added to requirements.txt
 import { motion, useViewportScroll, useTransform } from 'framer-motion/dist/framer-motion';
 
-const useStyles = makeStyles({});
+const useStyles = makeStyles({
+  images: {
+    position: 'fixed',
+    overflow: 'hidden',
+    top: '-40vw',
+    left: '0%',
+    width: '100%'
+  }
+});
 
 function img(url) {
   return 'https://hophacks-website.s3.amazonaws.com' + '/images/' + url;
@@ -31,19 +39,20 @@ function img(url) {
 //   );
 // }
 export default function Cover() {
-  const [imageLoading, setImageLoading] = useState(true);
-  const [pulsing, setPulsing] = useState(true);
+  //const [imageLoading, setImageLoading] = useState(true);
+  //const [pulsing, setPulsing] = useState(true);
   const { scrollYProgress } = useViewportScroll();
   //fit to screen size:1.165
-  const scale = useTransform(scrollYProgress, [0, 0.02], [4, 1.165]);
-  const opacity = useTransform(scrollYProgress, [0, 0.02, 0.08], [1, 1, 0]);
+  const scale = useTransform(scrollYProgress, [0, 0.08], [4, 1.165]);
+  const opacity = useTransform(scrollYProgress, [0, 0.08, 0.08], [1, 1, 0]);
   const mobileScale = useTransform(scrollYProgress, [0, 0.02], [10, 3.4]);
   const mobileOpacity = useTransform(scrollYProgress, [0, 0.02, 0.03], [1, 1, 0]);
-
-  const imageLoaded = () => {
-    setImageLoading(false);
-    setTimeout(() => setPulsing(false), 600);
-  };
+  //const yRight = useTransform(scrollYProgress, [0, 0.08], ['-70vh', '0vh']);
+  //const centerString = center.current + 'vw';
+  // const imageLoaded = () => {
+  //   setImageLoading(false);
+  //   setTimeout(() => setPulsing(false), 600);
+  // };
 
   const classes = useStyles();
   if (window.innerWidth <= 650) {
@@ -119,49 +128,31 @@ export default function Cover() {
             style={{
               scale,
               opacity
+              //top: yRight
             }}
           >
             <img
-              src={img('demo_sky.png')}
-              style={{
-                position: 'fixed',
-                overflow: 'hidden',
-                top: '0vw',
-                left: '0%',
-                width: '100%'
-              }}
+              src={img('sky.png')}
+              //style={{ top: center(scale.current) }}
+              className={classes.images}
               alt="image_could_not_load"
             />
             <img
-              src={img('demo_stage.png')}
-              style={{
-                position: 'fixed',
-                overflow: 'hidden',
-                top: '0vw',
-                left: '0%',
-                width: '100%'
-              }}
+              src={img('stage.png')}
+              //style={{ top: center(scale.current) }}
+              className={classes.images}
               alt="image_could_not_load"
             />
             <img
-              src={img('demo_crowds.png')}
-              style={{
-                position: 'fixed',
-                overflow: 'hidden',
-                top: '0vw',
-                left: '0%',
-                width: '100%'
-              }}
+              src={img('crowds.png')}
+              //style={{ top: center(scale.current) }}
+              className={classes.images}
               alt="image_could_not_load"
             />
             <img
-              src={img('demo_foreground.png')}
-              style={{
-                position: 'fixed',
-                top: '0vw',
-                left: '0%',
-                width: '100%'
-              }}
+              src={img('foreground.png')}
+              //style={{ top: center(scale.current) }}
+              className={classes.images}
               alt="image_could_not_load"
             />
 
