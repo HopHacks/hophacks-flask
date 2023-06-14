e6a8416c5b99163a90e540e23107b2801fa789e1
MuxiLyuLucy <muxilyu2004@gmail.com>
2023-06-14 17:13:17 +0800

    change team page bg

--- frontend/src/components/Team.jsx
+++ frontend/src/components/Team.jsx
@@ -23,16 +23,8 @@ export default function TeamPage() {
   );
 
   return (
-    <div
-      style={{
-        backgroundImage: `url("https://hophacks-website.s3.amazonaws.com/images/cover.png")`,
-        backgroundSize: 'cover',
-        minHeight: '100vh'
-      }}
-    >
-      <div className="container">
-        <div className="team-register-wrapper">{team}</div>
-      </div>
+    <div className="container">
+      <div className="team-register-wrapper">{team}</div>
     </div>
   );
 }
