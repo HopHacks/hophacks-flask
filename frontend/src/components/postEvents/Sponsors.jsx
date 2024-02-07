9fb45b634e7cd61f5c3e9080aa1992b0d657f776
Minoo Kim <minookim@minoos-mbp.acasmart.jh.edu>
2024-02-07 18:37:12 -0500

    added basic layout for sponsors page

--- frontend/src/components/postEvents/Sponsors.jsx
+++ frontend/src/components/postEvents/Sponsors.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import '../../stylesheets/sponsors.css';
 
 const Sponsors = () => {
     return (
-      <div style={{ color: 'white', fontSize: '24px' }}>
-        and most importantly, we want to thank...
+      <div className="page">
+        <div style={{
+            color: 'white',
+            fontSize: '36px',
+            textIndent: '30px',
+            paddingTop: "20px"
+        }}>
+            and most importantly, we want to thank...
+        </div>
       </div>
+     
     );
-};
+  };
   
 export default Sponsors;
