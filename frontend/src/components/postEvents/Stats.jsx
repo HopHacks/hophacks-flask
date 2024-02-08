06e6004bbdfee54173d439415ed1ead7b86d0bf8
clarajeon126 <clarajeon126@gmail.com>
2024-02-08 06:23:10 -0500

    boxes in stat

--- frontend/src/components/postEvents/Stats.jsx
+++ frontend/src/components/postEvents/Stats.jsx
@@ -4,9 +4,30 @@ import '../../stylesheets/stats.css';
 const Stats = () => {
   return (
     <div className='main'>
-      <div>1</div>
-      <div>2</div>
-      <div>3</div>
+      <div className='column' style={{flexGrow: 3}}>
+        <div className='horiBox'>fff</div>
+        <div className='horiBox' style={{flexGrow: 2}}>fff</div>
+        <div className='horiBoxEmpty'>
+            <div className='verBox'>fff</div>
+            <div className='verBox'>fff</div>
+        </div>
+      </div>
+      <div className='column' style={{flexGrow: 3}}>
+      <div className='horiBox'>fff</div>
+        <div className='horiBox' style={{flexGrow: 2, padding: 0}}>
+            <div className='mainStat'>
+                fff
+            </div>
+        </div>
+        <div className='horiBox'>fff</div>
+      </div>
+      <div className='column' >
+        <div className='horiBox' >fff</div>
+        <div className='horiBoxEmpty' style={{flexGrow: 3}}>
+            <div className='verBox'>fff</div>
+            <div className='verBox'>fff</div>
+        </div>
+      </div>
     </div>
   );
 };
