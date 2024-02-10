cf57de93904dc4e6f548e7c230051325986c7727
nishikar12 <nparuch1@jh.edu>
2024-02-10 17:54:44 -0500

    Revert "Merge pull request #349 from HopHacks/org_application"
    This reverts commit d3776675d09566ea413dd456bf6bb4bde7dd061c, reversing
    changes made to 42bd989fd20696502600ea00d0f23a6ec3486a92.

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -9,14 +9,12 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import { theme } from '../util/theme';
 import { AuthProvider } from '../util/auth';
 
-import Recruiting from './Recruiting';
-import Admin from './admin/Admin';
-import Nav from './Nav';
-import Team from './Team';
-/**
 import Home from './Home';
+import Recruiting from './Recruiting';
 import Profile from './account/Profile';
+import Admin from './admin/Admin';
 import AnnouncementPanel from './admin/AnnouncementPanel';
+import Nav from './Nav';
 import EmailConfirmation from './EmailConfirmation';
 import PasswordReset from './PasswordReset';
 import RSVP from './RSVP';
@@ -27,16 +25,17 @@ import SponsorPrizes from './judgetool/SponsorPrizes.jsx';
 import TablesAndRooms from './judgetool/TablesAndRooms.jsx';
 import Announcements from './announcement/Announcements.jsx';
 import AnnouncementDetails from './announcement/AnnouncementDetails.jsx';
+import Team from './Team';
 import Login from './register/Login';
 import SignUp from './register/signup/SignUp';
 import ResetPassword from './register/ResetPassword';
 import TeamMatchingPage from './team_matching/TeamMatchingPage';
+
 import { useMediaQuery } from 'usehooks-ts';
- */
 
 export default function App() {
-  // const isMobile = useMediaQuery('(max-width: 48em)');
-  // for organizer application
+  const isMobile = useMediaQuery('(max-width: 48em)');
+
   return (
     <>
       <ParallaxProvider>
@@ -45,23 +44,72 @@ export default function App() {
             <CssBaseline />
             <Router>
               <div>
-                <Nav mode="recruiting" />
+                <Nav />
 
                 <Switch>
+                  <Route path="/assignments" component={Assignments} />
+                  <Route path="/upload" component={Upload} />
+                  <Route path="/upload-sponsors" component={UploadSponsors} />
+                  <Route path="/sponsor-prizes" component={SponsorPrizes} />
+                  <Route path="/tables" component={TablesAndRooms} />
+
                   <Route exact path="/admin">
                     <Admin />
                   </Route>
+
+                  <Route exact path="/admin/announcementpanel">
+                    <AnnouncementPanel />
+                  </Route>
+
+                  <Route path="/profile">
+                    <Profile isMobile={isMobile} />
+                  </Route>
+
+                  <Route path="/announcements/detail">
+                    <AnnouncementDetails />
+                  </Route>
+
+                  <Route path="/announcements">
+                    <Announcements />
+                  </Route>
+
+                  {/* TODO: replace this with new register page */}
+                  {/* <Route path="/register">
+                    <Register isMobile ={isMobile}/>
+                  </Route> */}
 
                   <Route path="/recruiting">
                     <Recruiting />
                   </Route>
 
+                  <Route path="/rsvp">
+                    <RSVP />
+                  </Route>
+                  <Route path="/reset_password/:token" component={PasswordReset} />
+                  <Route path="/confirm_email/:token" component={EmailConfirmation} />
+
                   <Route path="/team">
                     <Team />
                   </Route>
 
+                  <Route path="/register/login">
+                    <Login isMobile={isMobile} />
+                  </Route>
+
+                  <Route path="/register/signup">
+                    <SignUp isMobile={isMobile} />
+                  </Route>
+
+                  <Route path="/register/resetpassword">
+                    <ResetPassword isMobile={isMobile} />
+                  </Route>
+
+                  <Route path="/teamMatching" isMobile={isMobile}>
+                    <TeamMatchingPage isMobile={isMobile} />
+                  </Route>
+
                   <Route path="/">
-                    <Recruiting />
+                    <Home />
                   </Route>
                 </Switch>
               </div>
@@ -71,88 +119,4 @@ export default function App() {
       </ParallaxProvider>
     </>
   );
-
-  // return (
-  //   <>
-  //     <ParallaxProvider>
-  //       <MuiThemeProvider theme={theme}>
-  //         <AuthProvider>
-  //           <CssBaseline />
-  //           <Router>
-  //             <div>
-  //               <Nav />
-
-  //               <Switch>
-  //                 <Route path="/assignments" component={Assignments} />
-  //                 <Route path="/upload" component={Upload} />
-  //                 <Route path="/upload-sponsors" component={UploadSponsors} />
-  //                 <Route path="/sponsor-prizes" component={SponsorPrizes} />
-  //                 <Route path="/tables" component={TablesAndRooms} />
-
-  //                 <Route exact path="/admin">
-  //                   <Admin />
-  //                 </Route>
-
-  //                 <Route exact path="/admin/announcementpanel">
-  //                   <AnnouncementPanel />
-  //                 </Route>
-
-  //                 <Route path="/profile">
-  //                   <Profile isMobile={isMobile} />
-  //                 </Route>
-
-  //                 <Route path="/announcements/detail">
-  //                   <AnnouncementDetails />
-  //                 </Route>
-
-  //                 <Route path="/announcements">
-  //                   <Announcements />
-  //                 </Route>
-
-  //                 {/* TODO: replace this with new register page */}
-  //                 {/* <Route path="/register">
-  //                   <Register isMobile ={isMobile}/>
-  //                 </Route> */}
-
-  //                 <Route path="/recruiting">
-  //                   <Recruiting />
-  //                 </Route>
-
-  //                 <Route path="/rsvp">
-  //                   <RSVP />
-  //                 </Route>
-  //                 <Route path="/reset_password/:token" component={PasswordReset} />
-  //                 <Route path="/confirm_email/:token" component={EmailConfirmation} />
-
-  //                 <Route path="/team">
-  //                   <Team />
-  //                 </Route>
-
-  //                 <Route path="/register/login">
-  //                   <Login isMobile={isMobile} />
-  //                 </Route>
-
-  //                 <Route path="/register/signup">
-  //                   <SignUp isMobile={isMobile} />
-  //                 </Route>
-
-  //                 <Route path="/register/resetpassword">
-  //                   <ResetPassword isMobile={isMobile} />
-  //                 </Route>
-
-  //                 <Route path="/teamMatching" isMobile={isMobile}>
-  //                   <TeamMatchingPage isMobile={isMobile} />
-  //                 </Route>
-
-  //                 <Route path="/">
-  //                   <Home />
-  //                 </Route>
-  //               </Switch>
-  //             </div>
-  //           </Router>
-  //         </AuthProvider>
-  //       </MuiThemeProvider>
-  //     </ParallaxProvider>
-  //   </>
-  // );
 }
