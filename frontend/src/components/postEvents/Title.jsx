454b49b8577bfb11cbe036ce54a1a1fbe4e6f478
Jin Hong Moon <jaydenmoon7@gmail.com>
2024-02-10 19:57:31 -0500

    post event group 2 title page done

--- frontend/src/components/postEvents/Title.jsx
+++ frontend/src/components/postEvents/Title.jsx
@@ -5,7 +5,6 @@ const Title = () => {
   return (
     <div className="title">
       <img src="/images/title_background.png" alt="Title Background" className="background-image" />
-      <img src="/images/logo2023.png" alt="Logo" className="logo" />
       <div className="centered-number-outline">2023</div>
       <div className="centered-number">2023</div>
     </div>
