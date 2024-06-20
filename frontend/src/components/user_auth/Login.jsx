b3cd98dac8cea5d0cf53409b2fcf62d196a2546f
nishikar12 <nparuch1@jh.edu>
2024-06-20 04:26:33 -0400

    fixed compiler error + eslint?

--- frontend/src/components/user_auth/Login.jsx
+++ frontend/src/components/user_auth/Login.jsx
@@ -77,6 +77,7 @@ function Login(props) {
             }}
           />
         </div>
+
         <div style={{ textAlign: 'right' }}>
           <Link to={'/register/resetpassword'}>
             <Typography class="card-text-blue"> forgot password? </Typography>
@@ -105,10 +106,10 @@ function Login(props) {
             use JHED
           </Button> */}
         </div>
+
         <Link to={'/register/signup'}>
           <Typography class="card-text-blue"> New To HopHacks? Sign Up Now! </Typography>
-        </Link>{' '}
-        */}
+        </Link>
       </CardContent>
     </Card>
   );
