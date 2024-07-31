88312635c7a860a21cc79532947e8a6ecf450efb
Ryan <ryan.wu365@gmail.com>
2024-07-31 14:13:07 -0400

    combining stuff

--- frontend/src/components/Sponsors.jsx
+++ frontend/src/components/Sponsors.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
-import { Button, Grid } from '@material-ui/core';
+import { Grid } from '@material-ui/core';
 const useStyles = makeStyles({
     margin: {
       marginBottom: '20px'
@@ -33,6 +33,20 @@ export default function Sponsors() {
         }}
       >
         Sponsors
+      </Typography>
+    );
+
+    const subtext = (
+      <Typography
+        variant="h5"
+        style={{
+          display: 'flex',
+          alignItems: 'left',
+          justifyContent: 'left',
+          height: '10%'
+        }}
+      >
+        Interested in sponsoring us? Email us at hophacks.sponsors@gmail.com 
       </Typography>
     );
 
@@ -167,57 +181,8 @@ export default function Sponsors() {
     const starterSponsor = (
         <img src={'https://hophacks-image.s3.amazonaws.com/hop.png'} style={starterStyle}/>
     );
-
-    const content = (
-      <Typography
-        className={classes.title}
-        variant="h4"
-        gutterBottom
-        style={{
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          height: '100%'
-        }}
-      >
-        <h4>
-          While we wait for HopHacks 2024, check <br></br> out some highlights from past years!
-        </h4>
-      </Typography>
-    );
-  
-    const year2023 = (
-      <Button
-        variant="contained"
-        style={{ height: '100px', width: '250px', backgroundColor: '#34b4eb' }}
-      >
-        <Typography variant="h3">2023</Typography>
-      </Button>
-    );
-    const year2022 = (
-      <Button
-        variant="contained"
-        style={{ height: '100px', width: '250px', backgroundColor: '#34b4eb' }}
-        color="blue"
-      >
-        <Typography variant="h3" color="blue">
-          2022
-        </Typography>
-      </Button>
-    );
-    const year2021 = (
-      <Button
-        variant="contained"
-        style={{ height: '100px', width: '250px', backgroundColor: '#34b4eb' }}
-        color="blue"
-      >
-        <Typography variant="h3">2021</Typography>
-      </Button>
-    );
   
     return (
-      //We should refactor this into separate importable components if we have time
-      //But this is fine for now
       <div>
         <div className="hero">
           <Grid
@@ -229,8 +194,15 @@ export default function Sponsors() {
               justifyContent: 'center'
             }}
           >
-            <Grid item xs={8} md={8}>
-              {title}
+            <Grid item>
+              <Grid container spacing={3}>
+                <Grid item xs={10} s={10} md={10}>
+                    {title}
+                </Grid>
+                <Grid item xs={10} s={10} md={10}>
+                    {subtext}
+                </Grid>
+              </Grid>
             </Grid>
             <Grid container spacing={3}>
                 <Grid item>
@@ -264,50 +236,8 @@ export default function Sponsors() {
                     {starterSponsor}
                 </Grid>
             </Grid>
-
-            {/* <Grid item xs={12} sm={12} md={12}>
-              <img
-                src={'https://hophacks-image.s3.amazonaws.com/hop.png'}
-                style={{ width: '200px', margin: '1px 100px 5px' }}
-              />
-            </Grid> */}
-            {/* <Grid item xs={8} md={8}>
-              <Grid
-                container
-                spacing={12}
-                style={{
-                  display: 'flex',
-                  alignItems: 'center',
-                  justifyContent: 'right',
-                  marginBottom: '13px'
-                }}
-              >
-                <Grid item xs={4} md={3}>
-                  {year2023}
-                </Grid>
-                <Grid item xs={4} md={3}>
-                  {year2022}
-                </Grid>
-                <Grid item xs={4} md={3}>
-                  {year2021}
-                </Grid>
-              </Grid>
-            </Grid> */}
           </Grid>
         </div>
-        {/* <div className="2023" style={{ display: 'flex', flexDirection: 'column' }}>
-          <h1 style={{ fontSize: '48px' }}>HOPHACKS 2023</h1>
-          <h1>SEPTEMBER 15-17</h1>
-          <a href="https://hophacks.devpost.com">
-            <h3>View the Devpost</h3>
-          </a>
-        </div>
-        <div className="stats" style={{ display: 'flex', justifyContent: 'center' }}>
-          <img
-            src={'https://hophacks-website.s3.amazonaws.com/images/stats2023.png'}
-            style={{ width: '1000px' }}
-          />
-        </div> */}
       </div>
     );
   }
