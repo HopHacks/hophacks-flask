5d9d4fac31a45848681cfed4a100e32ee9707cf5
Jin Hong Moon <jaydenmoon7@gmail.com>
2025-03-04 15:58:40 -0500

    Prettier fixed

--- frontend/src/components/account/Register.jsx
+++ frontend/src/components/account/Register.jsx
@@ -452,8 +452,7 @@ export default function Register() {
           <div style={{ fontSize: 15, textAlign: 'left', color: '#061A40' }}>
             <span>
               * I authorize you to share my application/registration information with Major League
-              Hacking for event administration, ranking, and MLH administration in-line with
-              the{' '}
+              Hacking for event administration, ranking, and MLH administration in-line with the{' '}
             </span>
             <Link onClick={openPrivacy}>MLH Privacy Policy</Link>
             <span>. I further agree to the terms of both the </span>
