e43d2ed8eb280f6ac834ebdc7812cbd353dc53dd
David <davidnbenjamin15@gmail.com>
2025-03-06 15:48:16 -0500

    Temp fix for login page formatting

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/components/home/Team.jsx',
-    './src/components/recap.jsx'
+  content: [
+    './src/components/home/Team.jsx',
+    './src/components/recap.jsx',
+    './src/components/user_auth/Login.jsx'
   ],
   theme: {
     extend: {}
