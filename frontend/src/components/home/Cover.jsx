af8c9e75b15c854378263bf3ffa0947ec88f103f
Jin Hong Moon <jaydenmoon7@gmail.com>
2025-04-03 11:48:12 -0400

    added footer, fixed discord link, made cover not absolute position

--- frontend/src/components/home/Cover.jsx
+++ frontend/src/components/home/Cover.jsx
@@ -1,98 +1,44 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import '../../stylesheets/cover.css';
 
-// import { motion, useViewportScroll, useTransform } from 'framer-motion';
-
-const useStyles = makeStyles({
-  images: {
-    // position: 'absolute',
-    overflow: 'hidden',
-    top: '0vw',
-    left: '0.05%',
-    width: '100%'
-  },
-  imagesNew: {
-    // position: 'absolute',
-    overflow: 'hidden',
-    top: '-27vw',
-    left: '0%',
-    width: '100%'
-  },
-  mobileImages: {
-    // position: 'absolute',
-    overflow: 'hidden',
-    //top: '-40vw',
-    left: '0%',
-    width: '100%'
-  }
-});
-
 export default function Cover() {
-  //const [imageLoading, setImageLoading] = useState(true);
-  //const [pulsing, setPulsing] = useState(true);
-  // const { scrollYProgress } = useViewportScroll();
-
-  // const mobileScale = useTransform(scrollYProgress, [0, 0.02], [0.35, 0.35]);
-  //const mobileScale = useTransform(scrollYProgress, [0, 0.02], [10, 3]);
-  // const mobileRegisterScale = useTransform(scrollYProgress, [0, 0.02], [0.13, 0.3]);
-  //const mobileOpacity = useTransform(scrollYProgress, [0, 0.02, 0.025, 0.06], [1, 1, 1, 0]);
-  // const mobileYRight = useTransform(scrollYProgress, [0, 0.02], ['20em', '40em']);
-  // const mobileRegisterYRight = useTransform(scrollYProgress, [0, 0.02], ['0em', '-3em']);
-
-  const classes = useStyles();
-  if (window.innerWidth <= 850 && 1 < 0) {
-    return (
+  return (
+    <section id="cover" className="cover-section">
       <div className="pageBackground">
-        <div className={classes.tempImageBackground}></div>
-      </div>
-    );
-  } else {
-    //const pos = useTransform(scrollYProgress, [0, 0.08, 0.16], ['fixed', 'fixed', 'absolute']);
-    //const backgroundYRight = useTransform(scrollYProgress, [0.04, 0.08], ['10vw', '40vw']);
-
-    // const registerYRight = useTransform(
-    //   scrollYProgress,
-    //   [0, 0.02, 0.04, 0.08],
-    //   ['0.3em', '-2em', '-4.5em', '-15em']
-    // );
-
-    return (
-      <section id="cover" className="cover-section">
-        <div className="pageBackground">
-          <div className="textHeaderContainer">
+        <div className="mainContent">
+          <div className="headerRow">
+            <div className="textLine">
+              <span className="larger-letter">H</span>OP
+            </div>
             <img
               src="https://hophacks-website.s3.us-east-1.amazonaws.com/images/website2025/hophacks2025logo.png"
               alt="HopHacks Logo"
               className="hophacks-logo"
             />
-            <div className="textHeader">
-              <span className="larger-letter">H</span>OP
-              <br />
+          </div>
+
+          <div className="headerRow">
+            <div className="textLine">
               <span className="larger-letter">H</span>ACKS
-              <div className="discordbtnContainer">
-                <a
-                  href="https://discord.gg/w2dWEuxY"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="discordbtn inline-block"
-                >
-                  <button className="join-discord">Join Discord!</button>
-                </a>
-              </div>
+            </div>
+            <div className="discordbtnContainer">
+              <a
+                href="https://discord.gg/8V8wmCWUhH"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="discordbtn inline-block"
+              >
+                <button className="join-discord">Join Discord!</button>
+              </a>
             </div>
           </div>
 
-          <div className="textSubtextContainer">
-            <div className="textSubtext">
-              <span className="larger-letter">I</span>LLUMINATING&nbsp;&nbsp;
-              <span className="larger-letter">I</span>NNOVATIONS
-            </div>
+          <div className="textSubtext">
+            <span className="larger-letter">I</span>LLUMINATING&nbsp;&nbsp;
+            <span className="larger-letter">I</span>NNOVATIONS
           </div>
 
-          <div className="detailsContainer">
-            <div className="details">September 12-14, 2025 · 250+ Hackers · 36 Hours</div>
-          </div>
+          <div className="details">September 12–14, 2025 · 250+ Hackers · 36 Hours</div>
 
           {/* TODO: This is a image "button"... but not a real button */}
           {/*
@@ -111,7 +57,7 @@ export default function Cover() {
 
           {/* <button> Register</button> */}
         </div>
-      </section>
-    );
-  }
+      </div>
+    </section>
+  );
 }
