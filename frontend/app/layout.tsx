7094d9d96b776e9d7e812f2b52e85c0ca473d15d
Ronit Sohal <ronitsohal@gmail.com>
2026-02-17 22:40:21 -0500

    new frontend directory created, and auth copied over

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,12 @@
+import "./globals.css";
+import { AuthProvider } from "./util/auth";
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>
+        <AuthProvider>{children}</AuthProvider>
+      </body>
+    </html>
+  );
+}
